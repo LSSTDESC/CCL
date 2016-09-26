0462fef982e58a02848f39856effd6de7f9b475e
Joe Zuntz <joezuntz@googlemail.com>
2016-09-26 14:57:07 +0100

    add some brief documentation on testing

--- tests/ccl_test_params.c
+++ tests/ccl_test_params.c
@@ -2,7 +2,7 @@
 #include "ctest.h"
 
 // We can define any constants we want to use in a set of tests here.
-// They are accessible as data->Omega_c, etc., in the test itself 
+// They are accessible as data->Omega_c, etc., in the tests themselves below. 
 // "params" is the name of the whole suite of tests.
 CTEST_DATA(params) {
     double Omega_c;
@@ -14,7 +14,7 @@ CTEST_DATA(params) {
     double w0;
 };
 
-// This function is one before each test defined below with CTEST2.
+// This function is one before each test defined below with CTEST2 in the suite.
 // It is used to set up any values needed by the tests.  The data
 // that can be passed to the tests are always in a struct called "data"
 // and defined above.
@@ -32,7 +32,11 @@ CTEST_SETUP(params){
 // the data defined above in CTEST_DATA and given values in CTEST_SETUP function.  
 // We could also define CTEST_TEARDOWN(params) that would be run after the tests.
 
-// This adds a new test called "create_lcdm" to the suite (collection) called "params"
+// This adds a new test called "create_lcdm" to the suite called "params".
+// There are a variety of different assertions available.
+
+// If you wanted to you could call other functions in here too and use these
+// assertions there also.
 CTEST2(params, create_lcdm){
     ccl_parameters params = ccl_parameters_create_flat_lcdm(data->Omega_c, data->Omega_b, data->h, data->A_s, data->n_s);
     ASSERT_DBL_NEAR_TOL(params.Omega_c, data->Omega_c, 1e-10);
@@ -41,6 +45,8 @@ CTEST2(params, create_lcdm){
     ASSERT_DBL_NEAR_TOL(params.Omega_n, 0.0, 1e-10);
 }
 
+// This adds a second test in the same suite.  It uses the same setup function as the
+// previous one (though the setup function is run afresh for each test).
 CTEST2(params, create_wacdm){
     ccl_parameters params = ccl_parameters_create_flat_lcdm(data->Omega_c, data->Omega_b, data->h, data->A_s, data->n_s);
     ASSERT_DBL_NEAR_TOL(params.Omega_c, data->Omega_c, 1e-10);
