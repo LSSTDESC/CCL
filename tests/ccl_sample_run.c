21c87a6bd0fafe0a4e65b6f478fe9df0d2f45500
vrastil <michalvrastil@seznam.cz>
2016-11-08 17:35:53 +0100

    updated example run

--- tests/ccl_sample_run.c
+++ tests/ccl_sample_run.c
@@ -21,19 +21,54 @@
 #define SZ_SH 0.05
 #define NL 500
 
+int ask_to_con(){
+	printf("Do you want to continue? (YES / NO)\n");
+	char in[10];
+	int i = 0;
+	while (true){
+		scanf("%10s", in);
+		if ((strcmp(in, "") == 0) || (strcmp(in, "yes") == 0) || (strcmp(in, "YES") == 0)) return 0;
+		else if ((strcmp(in, "no") == 0) || (strcmp(in, "NO") == 0)){printf("Exiting test...\n"); return 1;}
+		else if (i > 10){printf("Too many unrecongized options. Exiting test...\n"); return 1;}
+		else {printf("Unrecognized option.\n"); i++;}
+  }
+}
+
 int main(int argc,char **argv)
 {
-  printf("Example program which goes through basic of CCL library. For more information see file 'tutorial.md'\n\n");
+	int err;
+	printf("\n********************");
+	printf("\n* CCL TEST PROGRAM *");
+	printf("\n********************\n\n");
+	printf("* Example program which goes through basic of CCL library. For more information see file 'tutorial.md'\n*\n");
   
-  //Initialize cosmological parameters
-  printf("Initialize cosmological parameters with call for <ccl_parameters_create>\n\n");
-  printf("\tccl_parameters params=ccl_parameters_create(0.25,0.05,0.00,0.00,-1.00,0.00,0.70,2.1E-9,0.96,-1,NULL,NULL);\n\n");
-  ccl_parameters params=ccl_parameters_create(OC,OB,OK,ON,W0,WA,HH,AS,NS,-1,NULL,NULL);
-
-  //Initialize cosmology object given cosmo params
-  printf("Initialize cosmology with call for <ccl_cosmology_create>\n\n");
-  printf("\tccl_cosmology *cosmo=ccl_cosmology_create(params,default_config);\n\n");
-  ccl_cosmology *cosmo=ccl_cosmology_create(params,default_config);
-  
-  return 0;
+	//Initialize cosmological parameters
+	printf("* Initialize cosmological parameters with\n*\n");
+	printf("* ccl_parameters ccl_parameters_create(\n");
+	printf("*\tdouble Omega_c, double Omega_b, double Omega_k, double Omega_n, double w0, double wa, double h, double A_s, double n_s,\n");
+	printf("*\tint nz_mgrowth,double *zarr_mgrowth,double *dfarr_mgrowth);\n*\n\n");
+	printf("\tccl_parameters params = ccl_parameters_create (0.25,0.05,0.00,0.00,-1.00,0.00,0.70,2.1E-9,0.96,-1,NULL,NULL);\n\n*\n");
+	ccl_parameters params=ccl_parameters_create(OC,OB,OK,ON,W0,WA,HH,AS,NS,-1,NULL,NULL);
+	//if (ask_to_con()) return 0;
+	
+	//Initialize cosmology object given cosmo params
+	printf("* Initialize cosmology with call for\n*\n");
+	printf("* ccl_cosmology * ccl_cosmology_create(ccl_parameters params, ccl_configuration config);\n*\n\n");
+	printf("\tccl_cosmology *cosmo = ccl_cosmology_create (params,default_config);\n\n*\n");
+	ccl_cosmology *cosmo=ccl_cosmology_create(params,default_config);
+	//if (ask_to_con()) return 0;
+	
+	//Compute radial distance (see include/ccl_background.h for more routines)
+	printf("* With initialized comsology we can compute distances, growth factor or sigma_8.\n*\n");
+	printf("* \tdouble ccl_comoving_radial_distance(ccl_cosmology * cosmo, double a);\n");
+	printf("* \tdouble ccl_luminosity_distance(ccl_cosmology * cosmo, double a)\n");
+	printf("* \tdouble ccl_growth_factor_unnorm(ccl_cosmology * cosmo, double a);\n");
+	printf("* \tdouble ccl_sigma8(ccl_cosmology * cosmo);\n*\n");
+	
+	printf("* For our cosmological parameters, comoving distance to z = %.3lf is chi = %.3lf Mpc/h\n",ZD,ccl_comoving_radial_distance(cosmo,1./(1+ZD)));
+	printf("* (consistency check) Scale factor at chi = %.3lf Mpc/h is a = %.3lf.\n",
+		ccl_comoving_radial_distance(cosmo,1./(1+ZD)), ccl_scale_factor_of_chi(cosmo,ccl_comoving_radial_distance(cosmo,1./(1+ZD))));
+	printf("* Luminosity distance to z = %.3lf is chi = %.3lf Mpc/h\n",ZD,ccl_luminosity_distance(cosmo,1./(1+ZD)));
+	printf("* Growth factor and growth rate at z = %.3lf are D = %.3lf and f = %.3lf\n",ZD, ccl_growth_factor(cosmo,1./(1+ZD)),ccl_growth_rate(cosmo,1./(1+ZD)));
+	return 0;
 }
--- tutorial.md
+++ tutorial.md
@@ -1,13 +1,14 @@
 # Documentation
-This documentat contains basic information about used structures and functions. At the end of document is provided code which implements these basic function. See also test **ccl_sample_run.c**
+This documentat contains basic information about used structures and functions. At the end of document is provided code which implements these basic functions. See also test **ccl_sample_run.c**
 ### Cosmological parameters
 Start by defining cosmological parameters defined in structure **ccl_parameters**. This structure (exact definition in *include/ccl_core.h*) contains densities of matter, parameters of dark energy (*w0, wa*), Hubble parameters, primordial poer spectra, radiation parameters, derived parameters (*sigma_8, Omega_1, z_star*) and modified growth rate.
 
 You can initialize this structure through function **ccl_parameters_create** which returns object of type **ccl_parameters**.
-
-    ccl_parameters ccl_parameters_create(
-    double Omega_c, double Omega_b, double Omega_k, double Omega_n, double w0, double wa, double h, double A_s, double n_s,int nz_mgrowth,double *zarr_mgrowth,double *dfarr_mgrowth
-    );
+````c
+ccl_parameters ccl_parameters_create(
+double Omega_c, double Omega_b, double Omega_k, double Omega_n, double w0, double wa, double h, double A_s, double n_s,int nz_mgrowth,double *zarr_mgrowth,double *dfarr_mgrowth
+);
+````
 where:
 * Omega_c: cold dark matter
 * Omega_b: baryons
@@ -25,10 +26,10 @@ where:
 For some specific cosmologies you can also use functions **ccl_parameters_create_flat_lcdm, ccl_parameters_create_flat_wcdm, ccl_parameters_create_flat_wacdm, ccl_parameters_create_lcdm**, which automatically set some parameters. For more information, see file *include/ccl_core.c*
 ### Cosmology object
 For CCL`s functions you need an object of type **ccl_cosmology**, which can be initalize by function **ccl_cosmology_create**
-
-    ccl_cosmology * ccl_cosmology_create(ccl_parameters params, ccl_configuration config);
-
+````c
+ccl_cosmology * ccl_cosmology_create(ccl_parameters params, ccl_configuration config);
+````
 Note that the function returns pointer. Variable **params** of type **ccl_parameters** contains cosmological parameters created in previous step. Object **config** of type **ccl_configuration**. Structure **ccl_configuration** contains information about methods for computing transfer function, matter power spectrum and mass function (for available methods see *include/ccl_config.h*). For now, you should use default configuration **default_config**
-
-    const ccl_configuration default_config = {ccl_fitting_function, ccl_halofit, ccl_tinker};
-
+````c
+const ccl_configuration default_config = {ccl_fitting_function, ccl_halofit, ccl_tinker};
+````
