3dc2632ab58b3b791b70da99acbd98633b4a5652
Phil Bull <philbull@gmail.com>
2017-02-03 14:19:38 -0800

    Vastly simplify setup.py by getting swig to just link against the installed libccl C library. BREAKAGE: This introduces a new variable in setup.py called LIBCCL_LIBRARY_PATH. This must be filled by autotools with the eventual installed location of the libccl library. This has not been done yet though, so the installer is currently broken.

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@
 
 from distutils.core import *
 from distutils import sysconfig
-from distutils.command.build_ext import build_ext
-from distutils.command.install import install
 import os.path
 
 # Get numpy include directory (works across versions)
@@ -13,105 +11,19 @@ try:
 except AttributeError:
     numpy_include = numpy.get_numpy_include()
 
-# CLASS source directory
-CLASS_SRC_DIR = "class"
-
-# Default root data directory for CLASS (i.e. in the source dir)
-global CLASS_DATA_DIR
-CLASS_DATA_DIR = "class"
-
-# Override default 'install' and 'build_ext' behaviours so that the eventual 
-# installed location of the CLASS data directory can be passed to the compiler 
-# in advance. This is necessary because CLASS looks for certain datafiles in a 
-# directory that is hard-coded in the variable __CLASSDIR__ at compile-time. 
-# The program segfaults if this directory is not found.
-
-class Install(install):
-    
-    def run(self):
-        # Define global variable containing the location in which the CLASS 
-        # data will be installed (chosen to be in the same root dir as the 
-        # installed Python files, to keep everything together)
-        global CLASS_DATA_DIR
-        CLASS_DATA_DIR = os.path.join( self.install_libbase, 
-                                       self.distribution.get_name(),
-                                       "class" )
-        # Run the usual install script
-        install.run(self)
-        
-        # Copy datafiles from class subdirs to installed Python module dir
-        class_subdirs = ['bbn/', ]
-        for subdir in class_subdirs:
-            
-            # Define input/output dirs in OS-independent way
-            in_data_dir = os.path.join('class/', subdir)
-            out_data_dir = os.path.join(CLASS_DATA_DIR, subdir)
-            
-            # Copy datafiles
-            self.copy_tree(in_data_dir, out_data_dir)
-
-class BuildExt(build_ext):
-    
-    def build_extensions(self):
-        # Find Extension and modify relevant compiler argument to point at the 
-        # installed location of the CLASS data (uses global variable, which is 
-        # a bit of a hack)
-        for ext in self.extensions:
-            for i in range(len(ext.extra_compile_args)):
-                if "__CLASSDIR__" in ext.extra_compile_args[i]:
-                    ext.extra_compile_args[i] = \
-                                    "-D__CLASSDIR__=\"%s\"" % CLASS_DATA_DIR
-        
-        # Run the usual build_ext script
-        build_ext.build_extensions(self)
-
+LIBCCL_LIBRARY_PATH = "/home/phil/jpl/software/lib/"
 
 # CCL extension module
 _ccllib = Extension(
             "_ccllib",
-               ["pyccl/ccl.i",
-               
-                "src/ccl_core.c",
-                "src/ccl_background.c",
-                "src/ccl_cls.c",
-                "src/ccl_error.c",
-                "src/ccl_lsst_specs.c",
-                "src/ccl_massfunc.c",
-                "src/ccl_placeholder.c",
-                "src/ccl_utils.c",
-                "src/ccl_power.c",
-                
-                CLASS_SRC_DIR + "/source/background.c", 
-                CLASS_SRC_DIR + "/source/input.c", 
-                CLASS_SRC_DIR + "/source/lensing.c", 
-                CLASS_SRC_DIR + "/source/nonlinear.c", 
-                CLASS_SRC_DIR + "/source/output.c", 
-                CLASS_SRC_DIR + "/source/perturbations.c", 
-                CLASS_SRC_DIR + "/source/primordial.c", 
-                CLASS_SRC_DIR + "/source/spectra.c", 
-                CLASS_SRC_DIR + "/source/thermodynamics.c", 
-                CLASS_SRC_DIR + "/source/transfer.c", 
-                
-                CLASS_SRC_DIR + "/tools/arrays.c", 
-                CLASS_SRC_DIR + "/tools/common.c", 
-                CLASS_SRC_DIR + "/tools/dei_rkck.c", 
-                CLASS_SRC_DIR + "/tools/evolver_ndf15.c", 
-                CLASS_SRC_DIR + "/tools/evolver_rkck.c", 
-                CLASS_SRC_DIR + "/tools/growTable.c", 
-                CLASS_SRC_DIR + "/tools/hyperspherical.c", 
-                CLASS_SRC_DIR + "/tools/parser.c", 
-                CLASS_SRC_DIR + "/tools/quadrature.c", 
-                CLASS_SRC_DIR + "/tools/sparse.c", 
-               ],
-               libraries = ['m', 'gsl', 'gslcblas', 'gomp'],
+               ["pyccl/ccl.i",],
+               libraries = ['m', 'gsl', 'gslcblas', 'gomp', 'ccl'],
+               library_dirs = [LIBCCL_LIBRARY_PATH,],
+               runtime_library_dirs = [LIBCCL_LIBRARY_PATH,],
                include_dirs = [numpy_include, "include/", "class/include"],
-               extra_compile_args=['-O4', '-fopenmp', '-std=gnu99',
-                                   '-D__CLASSDIR__="%s"' % CLASS_DATA_DIR],
-               #extra_compile_args=['-g', '-O0', '-Q', '-std=gnu99'], # Debug
+               extra_compile_args=['-O4', '-fopenmp',],
                swig_opts=['-threads'],
            )
-           # N.B. Needs -std=gnu99 to allow for loop variables to be declared 
-           # in-line *and* for M_PI to be defined in math.h (it's not in C99)
 
 # CCL setup script
 setup(  name         = "pyccl",
@@ -120,6 +32,5 @@ setup(  name         = "pyccl",
         version      = "0.1",
         packages     = ['pyccl'],
         ext_modules  = [_ccllib,],
-        cmdclass     = {'install': Install, 'build_ext': BuildExt},
         )
 
