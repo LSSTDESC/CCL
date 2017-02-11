b744600bf23bc48df1f5e29d1ac2953d2b74b6c0
Phil Bull <philbull@gmail.com>
2017-02-11 15:12:58 -0800

    Fix bug with incorrect argument ordering in power spectrum fns

--- pyccl/power.py
+++ pyccl/power.py
@@ -4,11 +4,11 @@ from pyutils import _vectorize_fn, _vectorize_fn2
 
 def linear_matter_power(cosmo, a, k):
     return _vectorize_fn2(lib.linear_matter_power, 
-                          lib.linear_matter_power_vec, cosmo, a, k)
+                          lib.linear_matter_power_vec, cosmo, k, a)
 
 def nonlin_matter_power(cosmo, a, k):
     return _vectorize_fn2(lib.nonlin_matter_power, 
-                          lib.nonlin_matter_power_vec, cosmo, a, k)
+                          lib.nonlin_matter_power_vec, cosmo, k, a)
 
 def sigmaR(cosmo, R):
     return _vectorize_fn(lib.sigmaR, 
