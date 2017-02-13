733fca5db88908dc4301601d23091864f461c0b7
Phil Bull <philbull@gmail.com>
2017-02-13 14:05:12 -0800

    Add tests that cover most of the top-level interface

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,8 +1,12 @@
 from numpy.testing import run_module_suite
 
+# Per-module accuracy and input correctness tests
 from ccl_test_distances import *
 from ccl_test_growth import *
 from ccl_test_core import *
+
+# Overall interface functionality tests
+from ccl_test_pyccl_interface import *
 
 if __name__ == "__main__":
     # Run all tests
