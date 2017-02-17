6002f85e7f90e53e268b3e5f202c8bd45d626db7
fjaviersanchez <francs1@uci.edu>
2017-02-17 12:56:41 -0800

    Updated setup.py to install the C libraries automatically

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 from distutils.core import *
 from distutils import sysconfig
 import os.path
+from distutils.command.install import install as DistutilsInstall
 
 # Get numpy include directory (works across versions)
 import numpy
@@ -33,6 +34,21 @@ class PyTest(Command):
         errno = subprocess.call([sys.executable,'tests/run_tests.py'])
         raise SystemExit(errno)
 
+#Running the C installation beforehand
+class PyInstall(DistutilsInstall):
+    def run(self):
+        import sys,subprocess
+        errno = subprocess.call(['./configure'])
+        if(errno):
+            raise SystemExit(errno)
+        errno = subprocess.call(['make'])
+        if(errno):
+            raise SystemExit(errno)
+        errno = subprocess.call(['sudo', 'make', 'install'])
+        if(errno):
+            raise SystemExit(errno)
+        DistutilsInstall.run(self)
+
 # CCL setup script
 setup(  name         = "pyccl",
         description  = "Library of validated cosmological functions.",
@@ -40,5 +56,5 @@ setup(  name         = "pyccl",
         version      = "0.1",
         packages     = ['pyccl'],
         ext_modules  = [_ccllib,],
-        cmdclass = {'test': PyTest},
+        cmdclass = {'install': PyInstall, 'test': PyTest},
         )
