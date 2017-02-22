e11fa8b7c66c2cd6a686028f3843a1e4c60cebb9
Jérémy Neveu <jneveu@lal.in2p3.fr>
2017-02-22 19:03:31 +0100

    Modify vetorize_fn3 to check for scalar type of return

--- pyccl/pyutils.py
+++ pyccl/pyutils.py
@@ -159,5 +159,8 @@ def _vectorize_fn3(fn, fn_vec, cosmo, x, n, returns_status=True):
     
     # Check result and return
     check(status)
-    return f
+    if scalar :
+        return f[0]
+    else:
+        return f
     
