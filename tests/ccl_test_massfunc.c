cbe7d5961cc880ebc01d9dcd42aa3ea87cb6d41d
villarrealas <asv13@pitt.edu>
2017-03-14 10:14:16 -0400

    fixed test case to be more transparent

--- tests/ccl_test_massfunc.c
+++ tests/ccl_test_massfunc.c
@@ -99,6 +99,7 @@ static void compare_massfunc(int model, struct massfunc_data * data)
 
   double a = 1.0;
   double logmass = 10;
+  double odelta = 200;
   double rho_m = RHO_CRITICAL*cosmo->params.Omega_m*cosmo->params.h*cosmo->params.h;
 
   // compare to benchmark data
@@ -106,7 +107,7 @@ static void compare_massfunc(int model, struct massfunc_data * data)
     double mass = pow(10,logmass);
     double sigma_j = ccl_sigmaM(cosmo, mass, a, status);
     double loginvsigma_j = log10(1./sigma_j);
-    double logmassfunc_j = log10(ccl_massfunc(cosmo, mass, a, 200.,status)*mass/(rho_m*log(10.)));
+    double logmassfunc_j = log10(ccl_massfunc(cosmo, mass, a, odelta, status)*mass/(rho_m*log(10.)));
 
     double absolute_tolerance = SIGMA_TOLERANCE*data->massfunc[0][j];
     if (fabs(absolute_tolerance)<1e-12) absolute_tolerance = 1e-12;
