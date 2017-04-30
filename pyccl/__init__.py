6f9525f2696aa1a4de1fcff1d24dce802f6883ff
Elisa Chisari <elisa.chisari@physics.ox.ac.uk>
2017-04-30 18:42:41 +0100

    This branch incorporates configurable constants to python

--- pyccl/__init__.py
+++ pyccl/__init__.py
@@ -26,3 +26,5 @@ from lsst_specs import bias_clustering, sigmaz_clustering, sigmaz_sources, \
 # Useful constants and unit conversions
 from constants import CLIGHT_HMPC, MPC_TO_METER, PC_TO_METER, \
                       GNEWT, RHO_CRITICAL, SOLAR_MASS
+
+
