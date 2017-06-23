4ee03d1815eea824594b95546b61221e84e56c64
Elisa Chisari <elisa.chisari@physics.ox.ac.uk>
2017-06-23 02:46:51 +0100

    doxygen docs for ccl_lsst_specs.h

--- include/ccl_lsst_specs.h
+++ include/ccl_lsst_specs.h
@@ -15,15 +15,53 @@ typedef struct {
         void *  your_pz_params;
 } user_pz_info;
 
+/** A function to provide the bias of the clustering sample
+ *  This is an analytic function that gives the clustering bias
+ *  @param cosmo Cosmological parameters
+ *  @param a scale factor, normalized to 1 for today
+ *  @param status Status flag. 0 if there are no errors, nonzero otherwise.
+ *  @return b(z) for the clustering sample
+ */
 double ccl_specs_bias_clustering(ccl_cosmology * cosmo, double a, int * status); 
+
+/** Return dNdz in a particular tomographic bin, 
+    convolved with a photo-z model (defined by the user), and normalized.
+ * @param z redshift 
+ * @param dNdz_type the choice of dN/dz from Chang+
+ * @param bin_zmin the minimum redshift of the tomorgraphic bin
+ * @param bin_zmax the maximum redshift of the tomographic bin
+ * @param user_info the user P(z) info struct
+ * @param tomoout the output dN/dz
+ * @param status Status flag. 0 if there are no errors, nonzero otherwise.
+ * @return void 
+ */
 void ccl_specs_dNdz_tomog(double z, int dNdz_type, double bin_zmin, double bin_zmax, user_pz_info * user_info,  double *tomoout, int *status);
 
+/** This function creates a structure amalgamating the user-input information on the photo-z model, P(z) plus some parameters.
+ * @param user_params User-defined parameters for the P(z) function
+ * @param user_pz_func P(z) function
+ * @return a structure with the user-provided P(z) and parameters
+ */
 user_pz_info* ccl_specs_create_photoz_info(void * user_params, double(*user_pz_func)(double, double,void*,int*));
 
+/** Free memory holding the structure containing user-input photoz information.
+ * @param my_photoz_info that holds user-defined P(z) and parameters
+ * @return void
+ */
 void ccl_specs_free_photoz_info(user_pz_info *my_photoz_info);
 
+/** Return sigma(z), the photo-z dispersion, for the clustering sample
+    This is if you want to assume Gaussian uncertainties.
+ *  @param z redshift
+ *  @return sigma(z) for the clustering sample
+ */
 double ccl_specs_sigmaz_clustering(double z);
 
+/** Return sigma(z), the photo-z dispersion, for the lensing sample
+    This is if you want to assume Gaussian uncertainties.
+ *  @param z redshift
+ *  @return sigma(z) for the lensing sample
+ */
 double ccl_specs_sigmaz_sources(double z);
 
 // Specifying the dNdz
