348bd0f49a879e702567e6801cce131701fb0d1b
c-d-leonard <c.danielle.leonard@gmail.com>
2017-06-26 13:53:47 -0700

    doxygen documentation for neutrino files

--- include/ccl_neutrinos.h
+++ include/ccl_neutrinos.h
@@ -17,16 +17,26 @@
 // The combination of constants required in Omeganuh2
 #define NU_CONST (8. * pow(M_PI,5) *pow((KBOLTZ/ HPLANCK),3)* KBOLTZ/(15. *pow( CLIGHT,3))* (8. * M_PI * GNEWT) / (3. * 100.*100.*1000.*1000. /MPC_TO_METER /MPC_TO_METER  * CLIGHT * CLIGHT))
 
-// precalculate the phase space integral
+/**
+ * Spline for the phasespace integral required for getting the fractional energy density of massive neutrinos.
+ * Returns a gsl spline for the phase space integral needed for massive neutrinos.
+ * @param status Status flag. 0 if there are no errors, nonzero otherwise.
+ * For specific cases see documentation for ccl_error.c
+ * @return spl, the gsl spline for the phasespace integral required for massive neutrino calculations.
+ */
 gsl_spline* calculate_nu_phasespace_spline(int *status);
 
-/** Returns density of one neutrino species at a scale factor a.
- * @param a scale factor
- * @param Neff N effective
- * @param mnu neutrino mass
- * @param TCMB the temperature of the CMB
- * @param accel -
- * @param status status flag
+/** 
+ * Returns density of one neutrino species at a scale factor a. 
+ * Users are encouraged to access this quantity via the function ccl_omega_x.
+ * @param a Scale factor
+ * @param Neff The effective number of species with neutrino mass mnu.
+ * @param mnu Neutrino mass (can be 0).
+ * @param TCMB Temperature of the CMB
+ * @param accel - Interpolation accelerator to be used with phasespace spline. If not set yet, pass NULL.
+ * @param status Status flag. 0 if there are no errors, nonzero otherwise.
+ * For specific cases see documentation for ccl_error.c
+ * @return OmNuh2 Fractional energy density of neutrions with mass mnu, multiplied by h squared. 
  */
 double Omeganuh2 (double a, double Neff, double mnu, double TCMB, gsl_interp_accel* accel, int * status);
 
