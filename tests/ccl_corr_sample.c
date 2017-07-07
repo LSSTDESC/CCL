acdb3b1ffcb9dc895aa10f4172d9e4a2a7f75594
Phil Bull <philbull@gmail.com>
2017-07-06 18:10:16 -0700

    Add comments to ccl_corr_sample.c

--- tests/ccl_corr_sample.c
+++ tests/ccl_corr_sample.c
@@ -8,6 +8,7 @@
 #include <time.h>
 #include <string.h>
 
+// Define cosmological parameter values
 #define OC 0.25
 #define OB 0.05
 #define OK 0.00
@@ -31,49 +32,65 @@
 
 int main(int argc,char **argv)
 {
-
+  
+  // Use the default configuration, plus the cosmological parameters that were 
+  // defined above
   int status=0;
   ccl_configuration config = default_config;
-  ccl_parameters params=ccl_parameters_create(OC, OB, OK, NREL, NMAS, MNU, W0, WA, HH, NORMPS, NS,
-					      0,NULL,NULL, &status);
-  ccl_cosmology *cosmo=ccl_cosmology_create(params,config);
+  ccl_parameters params = ccl_parameters_create(OC, OB, OK, NREL, NMAS, MNU, 
+                                                W0, WA, HH, NORMPS, NS,
+					                            0, NULL, NULL, &status);
+  ccl_cosmology *cosmo = ccl_cosmology_create(params,config);
 
-  //Create tracers for angular power spectra
-  double z_arr_gc[NZ],nz_arr_gc[NZ],bz_arr[NZ];
-  for(int i=0;i<NZ;i++) {
-    z_arr_gc[i]=Z0_GC-5*SZ_GC+10*SZ_GC*(i+0.5)/NZ;
-    nz_arr_gc[i]=exp(-0.5*pow((z_arr_gc[i]-Z0_GC)/SZ_GC,2));
-    bz_arr[i]=1+z_arr_gc[i];
+  // Create example number density and bias for tracer
+  double z_arr_gc[NZ], nz_arr_gc[NZ], bz_arr[NZ];
+  for(int i=0; i < NZ; i++) {
+    z_arr_gc[i] = Z0_GC - 5.*SZ_GC + 10.*SZ_GC*(i + 0.5)/NZ;
+    nz_arr_gc[i] = exp(-0.5*pow((z_arr_gc[i] - Z0_GC) / SZ_GC, 2));
+    bz_arr[i] = 1. + z_arr_gc[i];
   }
 
-  //Galaxy clustering tracer
-  CCL_ClTracer *ct_gc=ccl_cl_tracer_number_counts_simple_new(cosmo,NZ,z_arr_gc,nz_arr_gc,
-							     NZ,z_arr_gc,bz_arr,&status);
-  
+  // Define a galaxy clustering tracer and calculate C_ell's
+  CCL_ClTracer *ct_gc = ccl_cl_tracer_number_counts_simple_new(
+                                cosmo, 
+                                NZ, z_arr_gc, nz_arr_gc,
+							    NZ, z_arr_gc, bz_arr,
+							    &status);
   int il;
-  double *clarr=malloc(ELL_MAX_CL*sizeof(double));
-  double *larr=malloc(ELL_MAX_CL*sizeof(double));
-  for(il=0;il<ELL_MAX_CL;il++){
-    larr[il]=il;
-    clarr[il]=ccl_angular_cl(cosmo,il,ct_gc,ct_gc,&status);
+  double *clarr = malloc(ELL_MAX_CL*sizeof(double));
+  double *larr = malloc(ELL_MAX_CL*sizeof(double));
+  for(il=0; il < ELL_MAX_CL; il++){
+    // Calculate auto-correlation of this tracer
+    larr[il] = il;
+    clarr[il] = ccl_angular_cl(cosmo, il, ct_gc, ct_gc, &status);
   }
+  
+  // Define cosine tapering, to reduce ringing. The first two numbers are 
+  // [lmin, lmax] for the low-ell taper, and the last two are [lmin, lmax] for 
+  // the high-ell taper.
+  double taper_cl_limits[4] = {1, 2, 10000, 15000};
+  
+  double *clustering_corr, *theta;
+  int ntheta = 15;
+  theta = ccl_log_spacing(0.01, 5., ntheta); // New array with log spacing
+  clustering_corr = malloc(ntheta*sizeof(double));
+  
+  // Calculate correlation function from angular power spectrum
+  ccl_correlation(cosmo, ELL_MAX_CL, larr, clarr, 
+                  ntheta, theta, clustering_corr, 
+                  CCL_CORR_GG, 0, taper_cl_limits, 
+                  CCL_CORR_FFTLOG, &status);
+  
+  // Print results
+  for(int it=0; it < ntheta; it++)
+    printf("%le %le\n", theta[it], clustering_corr[it]);
 
-  double *clustering_corr;
-  double *theta;
-  int ntheta=15;
-  double taper_cl_limits[4]={1,2,10000,15000};
-  theta = ccl_log_spacing(0.01,5.,ntheta);
-  clustering_corr=malloc(ntheta*sizeof(double));
-  ccl_correlation(cosmo,ELL_MAX_CL,larr,clarr,ntheta,theta,clustering_corr,CCL_CORR_GG,
-		  0,taper_cl_limits,CCL_CORR_FFTLOG,&status);
-
-  for(int it=0;it<ntheta;it++)
-    printf("%le %le\n",theta[it],clustering_corr[it]);
-
-  //Free up tracers
+  // Free tracers and other allocated memory
   ccl_cl_tracer_free(ct_gc);
-  //Free up cosmology
   ccl_cosmology_free(cosmo);
+  free(clustering_corr);
+  free(larr);
+  free(clarr);
 
   return 0;
 }
