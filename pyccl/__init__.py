852d4e354cc5450dca46d35bedcd353808edcafa
David Kirkby <dkirkby@uci.edu>
2017-07-12 13:52:29 -0400

    Fix line breaks

--- pyccl/__init__.py
+++ pyccl/__init__.py
@@ -7,22 +7,24 @@ from pyccl import core, constants, background, power, massfunction
 from pyccl.core import Parameters, Cosmology
 
 # Background cosmology functions and growth functions
-from pyccl.background import growth_factor, growth_factor_unnorm, growth_rate, \
-                       comoving_radial_distance, comoving_angular_distance, \
-                       h_over_h0, luminosity_distance, distance_modulus, \
-                       scale_factor_of_chi, omega_x
+from pyccl.background import growth_factor, growth_factor_unnorm, \
+    growth_rate, comoving_radial_distance, comoving_angular_distance, \
+    h_over_h0, luminosity_distance, distance_modulus, scale_factor_of_chi, \
+    omega_x
 
 # Power spectrum calculations and sigma8
-from pyccl.power import linear_matter_power, nonlin_matter_power, sigmaR, sigma8
+from pyccl.power import linear_matter_power, nonlin_matter_power, sigmaR, \
+    sigma8
 
 # Halo mass function
 from pyccl.massfunction import massfunc, massfunc_m2r, sigmaM, halo_bias
 
 # Cl's and tracers
-from pyccl.cls import angular_cl, ClTracer, ClTracerNumberCounts, ClTracerLensing
+from pyccl.cls import angular_cl, ClTracer, ClTracerNumberCounts, \
+    ClTracerLensing
 
-from pyccl.lsst_specs import bias_clustering, sigmaz_clustering, sigmaz_sources, \
-                       dNdz_tomog, PhotoZFunction
+from pyccl.lsst_specs import bias_clustering, sigmaz_clustering, \
+    sigmaz_sources, dNdz_tomog, PhotoZFunction
 
 # Useful constants and unit conversions
 from pyccl.constants import CLIGHT_HMPC, MPC_TO_METER, PC_TO_METER, \
