72d08d68d60f2bf0bb6bc3e7cd44dc1a50980e92
fjaviersanchez <francs1@uci.edu>
2017-07-12 19:16:43 -0700

    fixing travis problems

--- install-gsl.sh
+++ install-gsl.sh
@@ -2,6 +2,6 @@
 set -ex
 wget ftp://ftp.gnu.org/gnu/gsl/gsl-2.4.tar.gz
 tar -xzvf gsl-2.4.tar.gz
-cd gsl-2.4 && ./configure && make && sudo make install
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
-export LD_RUN_PATH=$LD_RUN_PATH:/usr/local/lib
+cd gsl-2.4 && ./configure --prefix=/usr && make && sudo make install
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib
+export LD_RUN_PATH=$LD_RUN_PATH:/usr/lib
