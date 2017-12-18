88df7aeba428e18a685df8c0e11e80fe71a85895
Tom McClintock <tmcclintock89@gmail.com>
2017-12-17 19:50:13 -0700

    Another docstring attempt.

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ if on_rtd:
         def __getattr__(cls, name):
             return MagicMock()
 
-    MOCK_MODULES = ['numpy','pyccl','cclib','_vectorize_fn','_vectorize_fn2','_vectorize_fn3']
+    MOCK_MODULES = ['numpy','pyccl']
     sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
