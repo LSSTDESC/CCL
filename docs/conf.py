ff5fd9a1c08632fbc923ae8ee2ad40b5fe0e3335
Tom McClintock <tmcclintock89@gmail.com>
2018-02-08 12:39:45 -0800

    Also mocking ccllib.

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ if on_rtd:
         def __getattr__(cls, name):
             return MagicMock()
 
-    MOCK_MODULES = ["_ccllib","numpy"]
+    MOCK_MODULES = ["_ccllib","numpy","ccllib"]
     #['pyccl','pyccl.background','pyccl.ccllib','pyccl.cls','pyccl.core','pyccl.constants','pyccl.correlation','pyccl.lsst_specs','pyccl.massfunction','pyccl.power','pyccl.pyutils']
     sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
