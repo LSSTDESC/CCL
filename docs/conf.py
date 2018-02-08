7feedc06cc1087506b50ead3beef5a8e1aee2a6d
Tom McClintock <tmcclintock89@gmail.com>
2018-02-08 13:55:58 -0800

    Cleaning up conf.py.

--- docs/conf.py
+++ docs/conf.py
@@ -110,7 +110,7 @@ todo_include_todos = True
 # a list of builtin themes.
 #
 import sphinx_rtd_theme
-html_theme = "sphinx_rtd_theme"#'alabaster'
+html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
