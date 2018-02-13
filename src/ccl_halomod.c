48e23c4f5fe4406dafae9b1306d93bcacaa6a91c
villarrealas <asv13@pitt.edu>
2018-02-13 13:20:58 -0500

    made some structure changes

--- src/ccl_halomod.c
+++ src/ccl_halomod.c
@@ -19,6 +19,9 @@
 #include "ccl_emu17.h"
 #include "ccl_emu17_params.h"
 
+// converts halo mass to rdelta.
+// TODO: possibly need to correct unit errors.
+// TODO: possible that delta should be passed around for consistency checks
 static double r_delta(ccl_cosmology *cosmo, double halomass, double a, int * status){
   double rho_m, delta;
   rho_m = RHO_CRITICAL*cosmo->params.Omega_m*cosmo->params.h*cosmo->params.h;
@@ -26,8 +29,6 @@ static double r_delta(ccl_cosmology *cosmo, double halomass, double a, int * sta
   return pow(halomass*3.0/(4.0*M_PI*rho_m*delta),1.0/3.0);
 }
 
-// TODO: rename parameters for consistency.
-// TODO: check if r_Delta is something equivalent in CCL.
 double u_nfw_c(ccl_cosmology *cosmo, double c,double halomass, double k, double a, int * status){
   // analytic FT of NFW profile, from Cooray & Sheth 2001
   double x, xu;
@@ -41,16 +42,12 @@ double u_nfw_c(ccl_cosmology *cosmo, double c,double halomass, double k, double 
   return (f1+f2-f3)/fc;
 }
 
-// TODO: we should probably set this up as a macro..
-double delta_c() {
-  //Linear collapse threshold
-  return 1.686;
-}
 
 // TODO: move this into ccl_massfunc as standard function conversion.
+// Alternatively - just use this in ccl_massfunc as necessary
 double nu(ccl_cosmology *cosmo, double halomass, double a, int * status) {
-  //nu = delta_c/sigma(M)
-  return delta_c()/ccl_sigmaM(cosmo,halomass,a,status);
+  double delta_c = 1.686;
+  return delta_c/ccl_sigmaM(cosmo,halomass,a,status);
 }
 
 // TODO: make consistency check so that ccl_halo_concentration only runs if called with appropriate definition
@@ -63,6 +60,7 @@ double ccl_halo_concentration(ccl_cosmology *cosmo, double halomass, double a, i
 }
 
 // TODO: move this into ccl_massfunc - be careful about the units!
+// Keep for now for immediate testing.
 double massfunc(double nu) {
   //Sheth Tormen mass function!
   //Note that nu=dc/sigma(M) and this Sheth & Tormen (1999) use nu=(dc/sigma)^2
@@ -73,6 +71,7 @@ double massfunc(double nu) {
   return A*(1.+pow(q*nu*nu,-p))*exp(-q*nu*nu/2.);
 }
 
+// TODO: serious simplification of this function.
 double inner_I0j (ccl_cosmology *cosmo, double halomass, double k, double a, void *para, int * status){
   double *array = (double *) para;
   long double u = 1.0; //the number one?
@@ -87,18 +86,26 @@ double inner_I0j (ccl_cosmology *cosmo, double halomass, double k, double a, voi
   return massfunc(nu(cosmo,halomass,a,status))*halomass*pow(halomass/(RHO_CRITICAL*cosmo->params.Omega_m),(double)j)*u;
 }
 
-// TODO: we need to change this to the CCL style integrator.
-// TODO: pass the cosmology construct.
-double I0j (int j, double k1, double k2, double k3, double k4,double a){
-  double array[7] = {k1,k2,k3,k4,0.,(double)j,a};
-//  return int_gsl_integrate_medium_precision(inner_I0j,(void*)array,log(limits.M_min),log(limits.M_max),NULL, 2000);
+double inner_I02(ccl_cosmology *cosmo, double halomass, double k, double a, int * status){
+  double c = ccl_halo_concentration(cosmo, halomass,a, status); //The halo concentration for this mass and scale factor
+  u = u_nfw_c(cosmo, c, halomass, k, a, status)*u_nfw_c(cosmo, c, halomass, k, a, status);
+  }
+  // TODO: mass function should be the CCL call - check units due to changes (Msun vs Msun/h, etc)
+  return massfunc(nu(cosmo,halomass,a,status))*halomass*pow(halomass/(RHO_CRITICAL*cosmo->params.Omega_m),2.0)*u;
+}
+
+double I02(ccl_cosmology *cosmo, double k, double a, int * status){
+  // TODO: here we will need to set up a structure that passes into the
+  // integration routines
+  double array[7] = {k1,k2,k3,k4,0.,2.0,a};
+//  return int_gsl_integrate_medium_precision(inner_I02,(void*)array,log(limits.M_min),log(limits.M_max),NULL, 2000);
   return 1.0; //temp while refactoring.
 }
 
 // TODO: pass the cosmology construct around.
-double p_1h(double k, double a)
+double p_1h(ccl_cosmology *cosmo, double k, double a, int * status)
 {
-  return I0j(2,k,k,0.,0.,a);
+  return I02(cosmo, k, a, status);
 }
 
 
