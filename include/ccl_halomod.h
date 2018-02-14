1b1bd8866a0bd11605ef83adf29bc40516944d38
villarrealas <asv13@pitt.edu>
2018-02-14 17:21:53 -0500

    quick addition to include for testing

--- include/ccl_halomod.h
+++ include/ccl_halomod.h
@@ -7,7 +7,16 @@ extern "C" {
 
 #include "ccl_core.h"
 
+double u_nfw_c(ccl_cosmology *cosmo, double c, double halomass, double k, double a, int * status);
+
+double nu(ccl_cosmology *cosmo, double halomass, double a, int * status);
+
+double massfunc_st(double nu);
+
+double I02(ccl_cosmology *cosmo, double k, double a, int * status);
+
 double p_1h(ccl_cosmology *cosmo, double k, double a, int * status);
+
 
 #ifdef __cplusplus
 }
