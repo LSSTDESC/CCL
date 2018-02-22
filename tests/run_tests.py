3dd7c821ec3427c7e461f746ae2c6410c977f943
Phil Bull <philbull@gmail.com>
2018-02-22 10:56:01 -0800

    Re-enable tests that were commented out

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -3,12 +3,12 @@ import sys
 
 # Per-module accuracy, input correctness, and unit tests
 from ccl_test_distances import *
-#from ccl_test_growth import *
-#from ccl_test_core import *
-#from ccl_test_power import *
+from ccl_test_growth import *
+from ccl_test_core import *
+from ccl_test_power import *
 
 # Overall interface functionality tests
-#from ccl_test_pyccl_interface import *
+from ccl_test_pyccl_interface import *
 
 if __name__ == "__main__":
     # Run all tests
