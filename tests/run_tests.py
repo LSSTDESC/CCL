d57a929c8d40f0fd3008894ada9b8c382a1182f3
Phil Bull <philbull@gmail.com>
2018-02-22 10:46:27 -0800

    Fix run_tests to allow nosetests to run on it. Temporary debug changes.

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,14 +1,15 @@
 from numpy.testing import run_module_suite
+import sys
 
 # Per-module accuracy, input correctness, and unit tests
 from ccl_test_distances import *
-from ccl_test_growth import *
-from ccl_test_core import *
-from ccl_test_power import *
+#from ccl_test_growth import *
+#from ccl_test_core import *
+#from ccl_test_power import *
 
 # Overall interface functionality tests
-from ccl_test_pyccl_interface import *
+#from ccl_test_pyccl_interface import *
 
 if __name__ == "__main__":
     # Run all tests
-    run_module_suite()
+    run_module_suite(argv=sys.argv)
