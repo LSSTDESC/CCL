c72ac442524019fb09fc8378e219c1f268ea622c
Francois <fr.eiffel@gmail.com>
2018-03-29 17:16:23 -0400

    Fizes travis script

--- .travis/install.sh
+++ .travis/install.sh
@@ -8,11 +8,14 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     case "${TOXENV}" in
         py27)
             # Install some custom Python 2.7 requirements on OS X
+            echo "No specific requirements on osx for now"
             ;;
         py36)
             # Install some custom Python 3.6 requirements on OS X
+            echo "No specific requirements on osx for now"
             ;;
     esac
 else
     # Install some custom requirements on Linux
+    echo "No specific requirements on linux"
 fi
