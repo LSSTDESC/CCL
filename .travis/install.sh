5eea38f33f13f0d25086d644501cde44e8fa0c04
Francois <fr.eiffel@gmail.com>
2018-03-29 19:40:07 -0400

    Fixed mac os x env

--- .travis/install.sh
+++ .travis/install.sh
@@ -9,14 +9,14 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
         py27)
             # Install some custom Python 2.7 requirements on OS X
             echo "No specific requirements on osx for now"
-            easy_install pip
+            easy_install --user pip
             pip install --user nose
             ;;
         py36)
             # Install some custom Python 3.6 requirements on OS X
             echo "No specific requirements on osx for now"
             brew update
-            brew install python3
+            brew upgrade python
             virtualenv venv -p python3
             source venv/bin/activate
             pip install --user nose numpy
