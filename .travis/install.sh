cddf6079110a4eeb05d97c973b1b02b98baf9532
Francois <fr.eiffel@gmail.com>
2018-03-29 20:20:53 -0400

    Switched everything to miniconda on osx

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,35 +3,37 @@
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 
     # Install some custom requirements on OS X
-    # e.g. brew install pyenv-virtualenv
+    if test -e $HOME/miniconda/bin; then
+      echo "miniconda already installed.";
+    else
+      echo "Installing miniconda.";
+      rm -rf $HOME/miniconda;
+      mkdir -p $HOME/download;
+      if [ "${TOXENV}" = py27 ]; then
+        wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O $HOME/download/miniconda.sh;
+      else
+        wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O $HOME/download/miniconda.sh;
+      fi;
+      bash $HOME/download/miniconda.sh -b -p $HOME/miniconda;
+    fi;
+    source $HOME/miniconda/bin/activate
+    hash -r
+    conda config --set always_yes yes --set changeps1 no
+    conda update -q conda
+    conda info -a
 
     case "${TOXENV}" in
         py27)
-            # Install some custom Python 2.7 requirements on OS X
-            echo "Native python environment"
-            sudo easy_install pip
-            pip install --user nose
+            conda create -q -n test-environment python=2.7 pip
             ;;
         py36)
-            echo "Installing miniconda.";
-            if test -e $HOME/miniconda/bin; then
-              echo "miniconda already installed.";
-            else
-              rm -rf $HOME/miniconda;
-              mkdir -p $HOME/download;
-              wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O $HOME/download/miniconda.sh;
-              bash $HOME/download/miniconda.sh -b -p $HOME/miniconda;
-            fi;
-            source $HOME/miniconda/bin/activate
-            hash -r
-            conda config --set always_yes yes --set changeps1 no
-            conda update -q conda
-            conda info -a
             conda create -q -n test-environment python=3.6 pip
-            source activate test-environment
-            pip install numpy nose
             ;;
-    esac
+    esac;
+
+    source activate test-environment
+    pip install numpy nose
+    
 else
     # Install some custom requirements on Linux
     echo "No specific requirements on linux"
