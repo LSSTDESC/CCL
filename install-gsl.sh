8b46a704bf16340ef292d41a54b2e8e47fdb8b2f
Phil Bull <philbull@gmail.com>
2018-04-08 16:43:11 -0700

    Remove debug lines from install-gsl script

--- install-gsl.sh
+++ install-gsl.sh
@@ -8,8 +8,5 @@ else
     tar -xzf gsl-2.4.tar.gz
     cd gsl-2.4 && ./configure --prefix=/usr && make && sudo make install
 fi
-pwd
-ls
-echo $TRAVIS_BUILD_DIR
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib
 export LD_RUN_PATH=$LD_RUN_PATH:/usr/lib
