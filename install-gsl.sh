8a1f48ac45f5750d10e61e76b09030971a603c0e
Phil Bull <philbull@gmail.com>
2018-04-08 12:17:45 -0700

    Temporarily add debug output for install-gsl

--- install-gsl.sh
+++ install-gsl.sh
@@ -8,5 +8,7 @@ else
     tar -xzf gsl-2.4.tar.gz
     cd gsl-2.4 && ./configure --prefix=/usr && make && sudo make install
 fi
+pwd
+ls
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib
 export LD_RUN_PATH=$LD_RUN_PATH:/usr/lib
