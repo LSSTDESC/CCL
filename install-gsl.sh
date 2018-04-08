e81f75e7856d38aeec6ebb6a3b3bdd397bcdadf6
Phil Bull <philbull@gmail.com>
2018-04-08 10:58:38 -0700

    Allow Travis to cache gsl

--- install-gsl.sh
+++ install-gsl.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 set -ex
-wget ftp://ftp.gnu.org/gnu/gsl/gsl-2.4.tar.gz
-tar -xzf gsl-2.4.tar.gz
-cd gsl-2.4 && ./configure --prefix=/usr && make && sudo make install
+if [ -e gsl-2.4.tar.gz ]
+then
+    wget ftp://ftp.gnu.org/gnu/gsl/gsl-2.4.tar.gz
+    tar -xzf gsl-2.4.tar.gz
+    cd gsl-2.4 && ./configure --prefix=/usr && make && sudo make install
+else
+    cd gsl-2.4 && sudo make install
+fi
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib
 export LD_RUN_PATH=$LD_RUN_PATH:/usr/lib
