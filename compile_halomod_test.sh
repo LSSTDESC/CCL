138de725431eec8926ece7a719250208edacfe3b
Alexander Mead <alexander.j.mead@googlemail.com>
2018-05-14 21:15:34 -0700

    moved my own test code elsewhere

--- compile_halomod_test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gcc -Wall -Wpedantic -g -O0 -I/usr/local/include -std=gnu99 -fPIC tests/ccl_test_halomod.c -o tests/ccl_test_halomod -L/usr/local/lib -lgsl -lgslcblas -lm -Lclass -lclass -lccl
