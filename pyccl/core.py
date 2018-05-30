c948c2c30a41291fb1d1d1effc15a1369a930ba6
Tilman Troester <tilman.troester@gmail.com>
2018-05-30 15:17:07 +0100

    Added meaningful error message when config file is missing. Fixed uninitialised parameters that were causing malloc errors during clean-up.

--- pyccl/core.py
+++ pyccl/core.py
@@ -66,20 +66,21 @@ mnu_types = {
 
 # Error types
 error_types = {
-    lib.CCL_ERROR_MEMORY:       'CCL_ERROR_MEMORY',
-    lib.CCL_ERROR_LINSPACE:     'CCL_ERROR_LINSPACE',
-    lib.CCL_ERROR_INCONSISTENT: 'CCL_ERROR_INCONSISTENT',
-    lib.CCL_ERROR_SPLINE:       'CCL_ERROR_SPLINE',
-    lib.CCL_ERROR_SPLINE_EV:    'CCL_ERROR_SPLINE_EV',
-    lib.CCL_ERROR_INTEG:        'CCL_ERROR_INTEG',
-    lib.CCL_ERROR_ROOT:         'CCL_ERROR_ROOT',
-    lib.CCL_ERROR_CLASS:        'CCL_ERROR_CLASS',
-    lib.CCL_ERROR_COMPUTECHI:   'CCL_ERROR_COMPUTECHI',
-    lib.CCL_ERROR_MF:           'CCL_ERROR_MF',
-    lib.CCL_ERROR_HMF_INTERP:   'CCL_ERROR_HMF_INTERP',
-    lib.CCL_ERROR_PARAMETERS:   'CCL_ERROR_PARAMETERS',
-    lib.CCL_ERROR_NU_INT:		'CCL_ERROR_NU_INT',
-    lib.CCL_ERROR_EMULATOR_BOUND: 'CCL_ERROR_EMULATOR_BOUND'
+    lib.CCL_ERROR_MEMORY:              'CCL_ERROR_MEMORY',
+    lib.CCL_ERROR_LINSPACE:            'CCL_ERROR_LINSPACE',
+    lib.CCL_ERROR_INCONSISTENT:        'CCL_ERROR_INCONSISTENT',
+    lib.CCL_ERROR_SPLINE:              'CCL_ERROR_SPLINE',
+    lib.CCL_ERROR_SPLINE_EV:           'CCL_ERROR_SPLINE_EV',
+    lib.CCL_ERROR_INTEG:               'CCL_ERROR_INTEG',
+    lib.CCL_ERROR_ROOT:                'CCL_ERROR_ROOT',
+    lib.CCL_ERROR_CLASS:               'CCL_ERROR_CLASS',
+    lib.CCL_ERROR_COMPUTECHI:          'CCL_ERROR_COMPUTECHI',
+    lib.CCL_ERROR_MF:                  'CCL_ERROR_MF',
+    lib.CCL_ERROR_HMF_INTERP:          'CCL_ERROR_HMF_INTERP',
+    lib.CCL_ERROR_PARAMETERS:          'CCL_ERROR_PARAMETERS',
+    lib.CCL_ERROR_NU_INT:		       'CCL_ERROR_NU_INT',
+    lib.CCL_ERROR_EMULATOR_BOUND:      'CCL_ERROR_EMULATOR_BOUND',
+    lib.CCL_ERROR_MISSING_CONFIG_FILE: 'CCL_ERROR_MISSING_CONFIG_FILE',
 }
 
 class Parameters(object):
@@ -433,10 +434,9 @@ class Cosmology(object):
         """Free the ccl_cosmology instance that this Cosmology object is managing.
 
         """
-        try:
-            lib.cosmology_free(self.cosmo)
-        except:
-            pass
+        if hasattr(self, "cosmo"):
+            if self.cosmo is not None:
+                lib.cosmology_free(self.cosmo)
     
     def __str__(self):
         """Output the cosmological parameters that were set, and their values,
