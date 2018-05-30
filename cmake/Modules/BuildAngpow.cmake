5b5a0f66faea874a525bf50ad7bb39977872d021
Francois <francois@orioncloud.fr>
2018-05-30 13:02:23 -0400

    Adds angpow compilation script

---
+++ cmake/Modules/BuildAngpow.cmake
@@ -0,0 +1,14 @@
+include(ExternalProject)
+
+set(AngpowTag v0.4)
+
+# Downloads and compiles Angpow
+ExternalProject_Add(Angpow
+        PREFIX ANGPOW
+        GIT_REPOSITORY https://github.com/LSSTDESC/Angpow4CCL.git
+        GIT_TAG ${AngpowTag}
+        DOWNLOAD_NO_PROGRESS 1
+        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/extern)
+set(ANGPOW_LIBRARY_DIRS ${CMAKE_BINARY_DIR}/extern/lib/ )
+set(ANGPOW_INCLUDE_DIRS ${CMAKE_BINARY_DIR}/extern/include/)
+set(ANGPOW_LIBRARIES -langpow)
