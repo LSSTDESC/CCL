e4d8ac4b9ec917f2f7913b6c61969050653ec26a
Francois <francois@orioncloud.fr>
2018-05-30 14:52:12 -0400

    Should support angpow now

--- cmake/Modules/BuildAngpow.cmake
+++ cmake/Modules/BuildAngpow.cmake
@@ -3,12 +3,12 @@ include(ExternalProject)
 set(AngpowTag v0.4)
 
 # Downloads and compiles Angpow
-ExternalProject_Add(Angpow
+ExternalProject_Add(ANGPOW
         PREFIX ANGPOW
         GIT_REPOSITORY https://github.com/LSSTDESC/Angpow4CCL.git
         GIT_TAG ${AngpowTag}
         DOWNLOAD_NO_PROGRESS 1
         CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/extern)
 set(ANGPOW_LIBRARY_DIRS ${CMAKE_BINARY_DIR}/extern/lib/ )
-set(ANGPOW_INCLUDE_DIRS ${CMAKE_BINARY_DIR}/extern/include/Angpow)
+set(ANGPOW_INCLUDE_DIRS ${CMAKE_BINARY_DIR}/extern/include/AngPow)
 set(ANGPOW_LIBRARIES -langpow -lstdc++)
