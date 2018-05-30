fa09971c6590003fd7552ce6aa9cb521d2578131
Francois <francois@orioncloud.fr>
2018-05-30 15:14:02 -0400

    Allow angpow to reuse the  GSL and FFTW compiled by CCL

--- cmake/Modules/BuildAngpow.cmake
+++ cmake/Modules/BuildAngpow.cmake
@@ -2,13 +2,16 @@ include(ExternalProject)
 
 set(AngpowTag v0.4)
 
+set(CMAKE_PREFIX_PATH "${CMAKE_BINARY_DIR}/libs/opencv-install/lib/pkgconfig")
+
 # Downloads and compiles Angpow
 ExternalProject_Add(ANGPOW
         PREFIX ANGPOW
         GIT_REPOSITORY https://github.com/LSSTDESC/Angpow4CCL.git
         GIT_TAG ${AngpowTag}
         DOWNLOAD_NO_PROGRESS 1
-        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/extern)
+        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/extern
+                   -DCMAKE_PREFIX_PATH=${CMAKE_BINARY_DIR}/extern/lib/pkgconfig)
 set(ANGPOW_LIBRARY_DIRS ${CMAKE_BINARY_DIR}/extern/lib/ )
 set(ANGPOW_INCLUDE_DIRS ${CMAKE_BINARY_DIR}/extern/include/AngPow)
 set(ANGPOW_LIBRARIES -langpow -lstdc++)
