9d74b2854a6d9c673803413aad2920ad52a5a610
Francois <francois@orioncloud.fr>
2018-05-31 08:48:04 -0400

    switch to official angpow version

--- cmake/Modules/BuildAngpow.cmake
+++ cmake/Modules/BuildAngpow.cmake
@@ -1,12 +1,12 @@
 include(ExternalProject)
 
-#set(AngpowTag v0.4)
+set(AngpowTag v0.4.1)
 
 # Downloads and compiles Angpow
 ExternalProject_Add(ANGPOW
         PREFIX ANGPOW
-        GIT_REPOSITORY https://github.com/EiffL/Angpow4CCL.git
-#        GIT_TAG ${AngpowTag}
+        GIT_REPOSITORY https://github.com/LSSTDESC/Angpow4CCL.git
+        GIT_TAG ${AngpowTag}
         DOWNLOAD_NO_PROGRESS 1
         CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/extern
                    -DCMAKE_PREFIX_PATH=${CMAKE_BINARY_DIR}/extern/lib/pkgconfig
