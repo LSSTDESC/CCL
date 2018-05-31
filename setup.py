988cc1bb56469c9dba4dea8696d2e1fec1e13bee
Francois <francois@orioncloud.fr>
2018-05-30 22:41:32 -0400

    Fixes issue when reading the readme

--- setup.py
+++ setup.py
@@ -25,9 +25,7 @@ class build(_build):
         _build.run(self)
 
 # read the contents of the README file
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open('README.md') as f:
     long_description = f.read()
 
 setup(name="pyccl",
