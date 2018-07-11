12c68887c43ff5feb20a7bb46134b42a79833629
David Alonso <dam.phys@gmail.com>
2018-07-11 17:36:28 +0100

    test rtd fix

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ if on_rtd:
         def __getattr__(cls, name):
             return MagicMock()
 
-    MOCK_MODULES = ["ccllib"]
+    MOCK_MODULES = ["pyccl.lib","pyccl.background.lib"]
     sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
