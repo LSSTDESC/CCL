0f1808fe76c539c5a41777144db3e21183dffd24
Matthew R Becker <becker.mr@gmail.com>
2018-10-05 11:55:26 -0500

    random change

--- tests/ccl_test_core.py
+++ tests/ccl_test_core.py
@@ -199,7 +199,6 @@ def test_no_c_cosmology_object():
     params = ccl.Parameters(Omega_c=0.25, Omega_b=0.05, h=0.7, A_s=2.1e-9,
                             n_s=0.96)
     cosmo = ccl.Cosmology(params=params)
-
     assert_raises(TypeError, ccl.core._cosmology_obj, cosmo.cosmo)
 
 
