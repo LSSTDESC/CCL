b53f7d4be235833344bf61b3f04417383b5a0c3f
Matthew R Becker <becker.mr@gmail.com>
2018-10-22 06:09:47 -0700

    tests of some c code

--- tests/ccl_test_utils.c
+++ tests/ccl_test_utils.c
@@ -14,14 +14,32 @@ CTEST(spacing_tests, linear_spacing_simple) {
   free(m);
 }
 
-
-CTEST(spacing_tests, linear_spacing_spline_limits) {
-  if(ccl_splines==NULL) ccl_cosmology_read_config();
-  double * m = ccl_linear_spacing(ccl_splines->A_SPLINE_MIN , ccl_splines->A_SPLINE_MAX,
-				  ccl_splines->A_SPLINE_NA);
+CTEST(spacing_tests, log_spacing_simple) {
+  double * m = ccl_log_spacing(1.0, 16.0, 5);
   ASSERT_NOT_NULL(m);
-  ASSERT_DBL_NEAR_TOL(ccl_splines->A_SPLINE_MIN, m[0], 1e-5);
-  ASSERT_DBL_NEAR_TOL(ccl_splines->A_SPLINE_MAX, m[ccl_splines->A_SPLINE_NA-1], 1e-5);
-  ASSERT_TRUE(m[ccl_splines->A_SPLINE_NA-1]<=1.0);
+  ASSERT_DBL_NEAR_TOL(1.0, m[0], 1e-10);
+  ASSERT_DBL_NEAR_TOL(2.0, m[1], 1e-10);
+  ASSERT_DBL_NEAR_TOL(4.0, m[2], 1e-10);
+  ASSERT_DBL_NEAR_TOL(8.0, m[3], 1e-10);
+  ASSERT_DBL_NEAR_TOL(16.0, m[4], 1e-10);
+  free(m);
+}
+
+CTEST(spacing_tests, linlog_spacing_simple) {
+  double * m = ccl_linlog_spacing(1.0, 16.0, 32.0, 5, 9);
+  ASSERT_NOT_NULL(m);
+  ASSERT_DBL_NEAR_TOL(1.0, m[0], 1e-10);
+  ASSERT_DBL_NEAR_TOL(2.0, m[1], 1e-10);
+  ASSERT_DBL_NEAR_TOL(4.0, m[2], 1e-10);
+  ASSERT_DBL_NEAR_TOL(8.0, m[3], 1e-10);
+  ASSERT_DBL_NEAR_TOL(16.0, m[4], 1e-10);
+  ASSERT_DBL_NEAR_TOL(18.0, m[5], 1e-10);
+  ASSERT_DBL_NEAR_TOL(20.0, m[6], 1e-10);
+  ASSERT_DBL_NEAR_TOL(22.0, m[7], 1e-10);
+  ASSERT_DBL_NEAR_TOL(24.0, m[8], 1e-10);
+  ASSERT_DBL_NEAR_TOL(26.0, m[9], 1e-10);
+  ASSERT_DBL_NEAR_TOL(28.0, m[10], 1e-10);
+  ASSERT_DBL_NEAR_TOL(30.0, m[11], 1e-10);
+  ASSERT_DBL_NEAR_TOL(32.0, m[12], 1e-10);
   free(m);
 }
