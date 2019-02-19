32c25ff68e031cd28a3dff8a129541a86465a20f
Matthew R Becker <becker.mr@gmail.com>
2019-02-18 23:02:35 -0600

    no cache

--- .travis/install.sh
+++ .travis/install.sh
@@ -7,35 +7,34 @@ else
 fi
 
 # Install some custom requirements on OS X
-if test -e $HOME/miniconda/bin; then
-  echo "miniconda already installed.";
+# if test -e $HOME/miniconda/bin; then
+#   echo "miniconda already installed.";
+# else
+echo "Installing miniconda.";
+rm -rf $HOME/miniconda;
+mkdir -p $HOME/download;
+if [ "${TOXENV}" = py27 ]; then
+wget https://repo.continuum.io/miniconda/Miniconda2-latest-${CONDA_INST}-x86_64.sh -O $HOME/download/miniconda.sh;
 else
-  echo "Installing miniconda.";
-  rm -rf $HOME/miniconda;
-  mkdir -p $HOME/download;
-  if [ "${TOXENV}" = py27 ]; then
-    wget https://repo.continuum.io/miniconda/Miniconda2-latest-${CONDA_INST}-x86_64.sh -O $HOME/download/miniconda.sh;
-  else
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-${CONDA_INST}-x86_64.sh -O $HOME/download/miniconda.sh;
-  fi;
-  bash $HOME/download/miniconda.sh -b -p $HOME/miniconda;
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-${CONDA_INST}-x86_64.sh -O $HOME/download/miniconda.sh;
+fi;
+bash $HOME/download/miniconda.sh -b -p $HOME/miniconda;
 
-  export PATH=$HOME/miniconda/bin:$PATH
+export PATH=$HOME/miniconda/bin:$PATH
 
-  conda config --set always_yes yes --set changeps1 no
-  conda config --add channels defaults
-  conda config --add channels conda-forge
-  conda update -q conda
-  conda info -a
+conda config --set always_yes yes --set changeps1 no
+conda config --add channels defaults
+conda config --add channels conda-forge
+conda update -q conda
+conda info -a
 
-  case "${TOXENV}" in
-    py27)
-      conda create -q -n test-environment python=2.7 pip \
-        numpy nose coveralls flake8 pyyaml gsl fftw cmake swig
-      ;;
-    py36)
-      conda create -q -n test-environment python=3.6 pip \
-        numpy nose coveralls flake8 pyyaml gsl fftw cmake swig
-      ;;
-  esac;
-fi
+case "${TOXENV}" in
+py27)
+  conda create -q -n test-environment python=2.7 pip \
+    numpy nose coveralls flake8 pyyaml gsl fftw cmake swig
+  ;;
+py36)
+  conda create -q -n test-environment python=3.6 pip \
+    numpy nose coveralls flake8 pyyaml gsl fftw cmake swig
+  ;;
+esac;
