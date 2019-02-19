864588ba6e03ae6826c75b21942a747e4d43436b
Matthew R Becker <becker.mr@gmail.com>
2019-02-18 22:56:38 -0600

    install once

--- .travis/install.sh
+++ .travis/install.sh
@@ -19,22 +19,23 @@ else
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-${CONDA_INST}-x86_64.sh -O $HOME/download/miniconda.sh;
   fi;
   bash $HOME/download/miniconda.sh -b -p $HOME/miniconda;
-fi;
-export PATH=$HOME/miniconda/bin:$PATH
 
-conda config --set always_yes yes --set changeps1 no
-conda config --add channels defaults
-conda config --add channels conda-forge
-conda update -q conda
-conda info -a
+  export PATH=$HOME/miniconda/bin:$PATH
 
-case "${TOXENV}" in
+  conda config --set always_yes yes --set changeps1 no
+  conda config --add channels defaults
+  conda config --add channels conda-forge
+  conda update -q conda
+  conda info -a
+
+  case "${TOXENV}" in
     py27)
-        conda create -q -n test-environment python=2.7 pip \
-            numpy nose coveralls flake8 pyyaml gsl fftw cmake swig
-        ;;
+      conda create -q -n test-environment python=2.7 pip \
+        numpy nose coveralls flake8 pyyaml gsl fftw cmake swig
+      ;;
     py36)
-        conda create -q -n test-environment python=3.6 pip \
-            numpy nose coveralls flake8 pyyaml gsl fftw cmake swig
-        ;;
-esac;
+      conda create -q -n test-environment python=3.6 pip \
+        numpy nose coveralls flake8 pyyaml gsl fftw cmake swig
+      ;;
+  esac;
+fi
