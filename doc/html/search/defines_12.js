a089e215c28a48f4581b62833c7ff973e8355917
Matthew R. Becker <beckermr@users.noreply.github.com>
2019-03-15 10:09:11 -0500

    DEP remove redshifts module (#579)

--- doc/html/search/defines_12.js
+++ doc/html/search/defines_12.js
@@ -1,6 +1,4 @@
 var searchData=
 [
-  ['tcmb',['TCMB',['../ccl__constants_8h.html#a439fe303ab3d3c94f8d0617cd3dab0bc',1,'ccl_constants.h']]],
-  ['teardown_5ffname',['TEARDOWN_FNAME',['../ctest_8h.html#a5c06c47bbeb5de1f37999f7b0eaad5ae',1,'ctest.h']]],
-  ['tncdm',['TNCDM',['../ccl__constants_8h.html#ae8f28193dc11b27b79a90bf83f982d43',1,'ccl_constants.h']]]
+  ['teardown_5ffname',['TEARDOWN_FNAME',['../ctest_8h.html#a5c06c47bbeb5de1f37999f7b0eaad5ae',1,'ctest.h']]]
 ];
--- doc/html/search/variables_0.js
+++ doc/html/search/variables_0.js
@@ -11,17 +11,18 @@ var searchData=
   ['a_5fspline_5fna_5fpk',['A_SPLINE_NA_PK',['../structccl__spline__params.html#a1e8edb8505e8a05d62df4d92999dacfc',1,'ccl_spline_params']]],
   ['a_5fspline_5fnlog',['A_SPLINE_NLOG',['../structccl__spline__params.html#a53ec49c7af23118fae9868df9569ff72',1,'ccl_spline_params']]],
   ['a_5fspline_5fnlog_5fpk',['A_SPLINE_NLOG_PK',['../structccl__spline__params.html#afebd711662e73360a7b18968ac5e8f40',1,'ccl_spline_params']]],
+  ['a_5fspline_5ftype',['A_SPLINE_TYPE',['../structccl__spline__params.html#af2971beb5a86401be3dc4b6533ec0650',1,'ccl_spline_params']]],
   ['accelerator',['accelerator',['../structccl__data.html#ad8d2aef21dc9cf1fdee8e4afe3778b33',1,'ccl_data']]],
   ['accelerator_5fachi',['accelerator_achi',['../structccl__data.html#a813c89f4e504a5954b47d7b7797c49a3',1,'ccl_data']]],
   ['accelerator_5fd',['accelerator_d',['../structccl__data.html#aef120c743423fc4967e56fe0f60b52c8',1,'ccl_data']]],
   ['accelerator_5fk',['accelerator_k',['../structccl__data.html#af4d4a86009087a7fd3013885cc2811b1',1,'ccl_data']]],
   ['accelerator_5fm',['accelerator_m',['../structccl__data.html#a80406bf4aa0f060bedc245b2b960c547',1,'ccl_data']]],
   ['achi',['achi',['../structccl__data.html#a0b1087588bd517142a5b6f8c352218e0',1,'ccl_data']]],
-  ['alpha',['alpha',['../structsmail__params.html#acd8cd1e6aa420da9133e41207c675811',1,'smail_params']]],
   ['alphab',['alphab',['../structeh__struct.html#a623316721501ca221093de25e216ac9f',1,'eh_struct']]],
   ['alphac',['alphac',['../structeh__struct.html#a58de15592e924e87fe1b838599579a70',1,'eh_struct']]],
   ['alphahmf',['alphahmf',['../structccl__data.html#ad7fad40bf36fed46f5879104247de00b',1,'ccl_data']]],
+  ['amax',['amax',['../structccl__p2d__t.html#af090a647ddaaa73a69aadf73d6ae2bb5',1,'ccl_p2d_t']]],
+  ['amin',['amin',['../structccl__p2d__t.html#a9dd0557e6139d5560deb590157df53e3',1,'ccl_p2d_t']]],
   ['args',['args',['../namespacerun__tests.html#a5b1510476d0793c7c91a6e0f733d7c29',1,'run_tests']]],
-  ['argv',['argv',['../namespaceccl__test__power.html#a1b5fdf14c4f06d4a8722a7c41d8dbe1a',1,'ccl_test_power.argv()'],['../namespacerun__tests.html#a8a7b00d6f8aa6bda446535ba55c777fe',1,'run_tests.argv()']]],
-  ['avec',['avec',['../namespacetest__neutrino3p04__emu.html#ab8c4bce75a1656e3bd59c06088e516eb',1,'test_neutrino3p04_emu']]]
+  ['argv',['argv',['../namespaceccl__test__power.html#a1b5fdf14c4f06d4a8722a7c41d8dbe1a',1,'ccl_test_power.argv()'],['../namespacerun__tests.html#a8a7b00d6f8aa6bda446535ba55c777fe',1,'run_tests.argv()']]]
 ];
