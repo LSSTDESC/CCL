f1125e15e8d06ea046edbd20ace950cad7ccd7f4
Matthew R Becker <becker.mr@gmail.com>
2019-07-31 00:22:31 -0500

    hrm

--- .travis/install.sh
+++ .travis/install.sh
@@ -32,13 +32,11 @@ case "${TOXENV}" in
 py27)
   conda create -q -n test-environment python=2.7 pip \
     numpy nose coveralls flake8 pyyaml gsl fftw cmake swig scipy \
-    compilers pkg-config setuptools_scm pytest pandas python-coveralls \
-    pytest-cov
+    compilers pkg-config setuptools_scm pytest pandas pytest-cov
   ;;
 py36)
   conda create -q -n test-environment python=3.6 pip \
     numpy nose coveralls flake8 pyyaml gsl fftw cmake swig  scipy \
-    compilers pkg-config setuptools_scm pytest pandas python-coveralls \
-    pytest-cov
+    compilers pkg-config setuptools_scm pytest pandas pytest-cov
   ;;
 esac;
