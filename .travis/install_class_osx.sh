84c086f186716367bb1403841865c53a2ccc103f
Matthew R Becker <becker.mr@gmail.com>
2019-08-19 12:05:36 -0500

    grrr

--- .travis/install_class_osx.sh
+++ .travis/install_class_osx.sh
@@ -3,6 +3,12 @@
 git clone https://github.com/lesgourg/class_public.git
 cd class_public
 git checkout v2.7.2
+
+sed 's/"CC       = gcc"/""/g' Makefile
+sed 's/"AR        = ar rv"/""/g' Makefile
+
+cat Makefile
+
 make
 
 # at this point the make file leaves you in the python dir
