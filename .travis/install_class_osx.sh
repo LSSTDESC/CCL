d279bb79f2a0c78f46cafa5c543d8478df05b890
Matthew R Becker <becker.mr@gmail.com>
2019-08-19 12:40:06 -0500

    working osx install

--- .travis/install_class_osx.sh
+++ .travis/install_class_osx.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-git clone https://github.com/lesgourg/class_public.git
+export CONDA_BUILD_SYSROOT=/
+
+rm -rf class_public
+git clone --depth=1000 https://github.com/lesgourg/class_public.git
 cd class_public
 git checkout v2.7.2
 
-sed 's/"CC       = gcc"/""/g' Makefile
-sed 's/"AR        = ar rv"/""/g' Makefile
+sed -i.bak -e 's/^CC/#CC/g' Makefile
+sed -i.bak -e 's/^OPTFLAG =/OPTFLAG = -I${CONDA_PREFIX}\/include/g' Makefile
+sed -i.bak -e 's/^#CCFLAG +=/CCFLAG +=/g' Makefile
+sed -i.bak -e 's/^#CCFLAG =/CCFLAG =/g' Makefile
 
 cat Makefile
 
 make
 
 # at this point the make file leaves you in the python dir
-python setup.py install
-
 cd ..
