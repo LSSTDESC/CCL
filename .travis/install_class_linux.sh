909b679f44584aa12bd9b383a34037fe108cf0e1
Matthew R Becker <becker.mr@gmail.com>
2019-08-19 11:45:09 -0500

    again

---
+++ .travis/install_class_linux.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git clone https://github.com/lesgourg/class_public.git
+cd class_public
+git checkout v2.7.2
+make
+
+# at this point the make file leaves you in the python dir
+python setup.py install
+
+cd ..
+rm -rf class_public
