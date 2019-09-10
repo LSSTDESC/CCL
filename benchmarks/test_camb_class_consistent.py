d18baffc596c604283a35e0e2fafbd85d970a676
Matthew R Becker <becker.mr@gmail.com>
2019-09-10 06:39:39 -0500

    added camb vs class benchmark

---
+++ benchmarks/test_camb_class_consistent.py
@@ -0,0 +1,30 @@
+import pytest
+import numpy as np
+
+import pyccl as ccl
+
+
+@pytest.mark.parametrize('kwargs', [
+    dict(m_nu=[0.2, 0.1, 0.5], w0=-0.8, wa=0.2),
+    dict(m_nu=[0.2, 0.1, 0.0]),
+    dict(m_nu=0.5),
+    dict(w0=-0.8, wa=0.2),
+    dict()])
+def testcamb_class_consistent(kwargs):
+    c_camb = ccl.Cosmology(
+        Omega_c=0.25, Omega_b=0.05, h=0.7, n_s=0.95, A_s=2e-9,
+        transfer_function='boltzmann_camb', **kwargs)
+
+    c_class = ccl.Cosmology(
+        Omega_c=0.25, Omega_b=0.05, h=0.7, n_s=0.95, A_s=2e-9,
+        transfer_function='boltzmann_camb', **kwargs)
+
+    assert np.allclose(
+        ccl.sigma8(c_camb), ccl.sigma8(c_class), atol=0, rtol=2e-3)
+
+    a = 0.845
+    k = np.logspace(-4, 1, 100)
+    pk_camb = ccl.linear_matter_power(c_camb, k, a)
+    pk_class = ccl.linear_matter_power(c_class, k, a)
+
+    assert np.allclose(pk_camb, pk_class, atol=0, rtol=2e-3)
--- pyccl/boltzmann.py
+++ pyccl/boltzmann.py
@@ -32,7 +32,7 @@ def get_camb_pk_lin(cosmo):
     """
 
     assert HAVE_CAMB, (
-        "You must have the python packahe for CAMB "
+        "You must have the `camb` python package "
         "installed to run CCL with CAMB!")
 
     # z sampling from CCL parameters
@@ -40,46 +40,100 @@ def get_camb_pk_lin(cosmo):
     status = 0
     a_arr, status = lib.get_pk_spline_a(cosmo.cosmo, na, status)
     check(status)
+    a_arr = np.sort(a_arr)
     zs = 1.0 / a_arr - 1
     zs = np.clip(zs, 0, np.inf)
-    assert np.allclose(zs[-1], 0)
 
-    # init camb params
-    h2 = cosmo['h']**2
-    kwargs = dict(
-        WantTransfer=True,  # compute the matter transfer function
-        NonLinear=camb.model.NonLinear_none,  # only linear P(k)
-        ns=cosmo['n_s'],
-        H0=cosmo['h'] * 100,
-        ombh2=cosmo['Omega_b'] * h2,
-        omch2=cosmo['Omega_c'] * h2,
-        omk=cosmo['Omega_k'],
-        w=cosmo['w0'],
-        wa=cosmo['wa'],
-        dark_energy_model='DarkEnergyFluid',
-        kmax=10,
-        redshifts=[_z for _z in zs],
-        # FIXME - neutrinos!
-    )
-
+    # deal with normalization
     if np.isfinite(cosmo["A_s"]):
-        kwargs["As"] = cosmo["A_s"]
+        A_s_fid = cosmo["A_s"]
     elif np.isfinite(cosmo["sigma8"]):
         # in this case, CCL will internally normalize for us when we init
         # the linear power spectrum - so we just get close
         A_s_fid = 2.43e-9 * (cosmo["sigma8"] / 0.87659)**2
-        kwargs["As"] = A_s_fid
     else:
         raise CCLError(
             "Could not normalize the linear power spectrum! "
             "A_s = %f, sigma8 = %f" % (
                 cosmo['A_s'], cosmo['sigma8']))
 
-    camb_params = camb.set_params(**kwargs)
-    camb_res = camb.get_results(camb_params)
+    # init camb params
+    h2 = cosmo['h']**2
+    kwargs = dict(
+        # basic background stuff
+        H0=cosmo['h'] * 100,
+        ombh2=cosmo['Omega_b'] * h2,
+        omch2=cosmo['Omega_c'] * h2,
+        omk=cosmo['Omega_k'],
 
+        # neutrinos
+        omnuh2=cosmo['Omega_n_mass'] * h2,
+        nnu=cosmo['Neff'],
+        standard_neutrino_neff=cosmo['Neff'],
+        # CAMB and CLASS do slightly different things here
+        # CLASS has extra factors in the relationship between the CMB and
+        # neutrino temperature, which causes cosmo['N_nu_rel'] to not be
+        # exactly cosmo['Neff'] - cosmo['N_nu_mass'].
+        # CAMB internally does not do this, so we are left with having
+        # to fudge things a bit here. :/
+        num_nu_massless=cosmo['Neff'] - cosmo['N_nu_mass'],
+        num_nu_massive=int(cosmo['N_nu_mass']),
+        share_delta_neff=True,
+
+        # dark energy
+        dark_energy_model='fluid',
+        w=cosmo['w0'],
+        wa=cosmo['wa'],
+
+        # matter P(k) outputs
+        redshifts=[_z for _z in zs],
+        kmax=10,
+        nonlinear=False,  # no halofit
+        WantTransfer=True,  # compute the matter transfer function
+
+        # ICs
+        As=A_s_fid,
+        ns=cosmo['n_s'],
+
+        # "constants"
+        TCMB=cosmo['T_CMB'],
+    )
+
+    if cosmo['N_nu_mass'] > 0:
+        nu_mass_fracs = cosmo['mnu'][:cosmo['N_nu_mass']]
+        nu_mass_fracs = nu_mass_fracs / np.sum(nu_mass_fracs)
+        kwargs['nu_mass_eigenstates'] = int(cosmo['N_nu_mass'])
+        kwargs['nu_mass_numbers'] = np.ones(cosmo['N_nu_mass'], dtype=np.int)
+        kwargs['nu_mass_fractions'] = nu_mass_fracs
+        kwargs['nu_mass_degeneracies'] = (
+            np.ones(int(cosmo['N_nu_mass'])) *
+            cosmo['Neff'] / int(cosmo['Neff']))
+
+    cp = camb.set_params(**kwargs)
+
+    # internally CAMB will try to set these using some default
+    # assumptions that we do not want
+    # thus we reset them here
+    if cosmo['N_nu_mass'] > 0:
+        cp.nu_mass_degeneracies = (
+            np.ones(int(cosmo['N_nu_mass'])) *
+            cosmo['Neff'] / int(cosmo['Neff']))
+        nu_mass_fracs = cosmo['mnu'][:cosmo['N_nu_mass']]
+        nu_mass_fracs = nu_mass_fracs / np.sum(nu_mass_fracs)
+        cp.nu_mass_eigenstates = int(cosmo['N_nu_mass'])
+        cp.nu_mass_numbers = np.ones(cosmo['N_nu_mass'], dtype=np.int)
+        cp.nu_mass_fractions = nu_mass_fracs
+    else:
+        cp.nu_mass_eigenstates = 0
+        cp.nu_mass_numbers = []
+        cp.nu_mass_fractions = []
+        cp.nu_mass_degeneracies = []
+
+    # run CAMB and get results
+    camb_res = camb.get_results(cp)
     k, z, pk = camb_res.get_linear_matter_power_spectrum(
         hubble_units=True, nonlinear=False)
+
     # convert to non-h inverse units
     k *= cosmo['h']
     pk /= (h2 * cosmo['h'])
@@ -87,12 +141,10 @@ def get_camb_pk_lin(cosmo):
     # now build interpolant
     nk = k.shape[0]
     lk_arr = np.log(k)
-
     a_arr = 1.0 / (1.0 + z)
-    assert a_arr.shape[0] == na
+    na = a_arr.shape[0]
     sinds = np.argsort(a_arr)
     a_arr = a_arr[sinds]
-
     ln_p_k_and_z = np.zeros((na, nk), dtype=np.float64)
     for i, sind in enumerate(sinds):
         ln_p_k_and_z[i, :] = np.log(pk[sind, :])
@@ -106,6 +158,7 @@ def get_camb_pk_lin(cosmo):
         extrap_order_lok=1,
         extrap_order_hik=2,
         cosmo=cosmo)
+
     return pk_lin
 
 
