504fbc9b22c8d6beae42588e4fb94bd0ad2065f5
David Alonso <dam.phys@gmail.com>
2019-11-10 18:15:21 +0000

    final tests

--- pyccl/tests/test_hmfunc.py
+++ pyccl/tests/test_hmfunc.py
@@ -20,6 +20,8 @@ MS = [1E13, [1E12, 1E15], np.array([1E12, 1E15])]
 MFOF = ccl.halos.MassDef('fof', 'matter')
 MVIR = ccl.halos.MassDef('vir', 'critical')
 M100 = ccl.halos.MassDef(100, 'matter')
+M200c = ccl.halos.MassDef(200, 'critical')
+M200m = ccl.halos.MassDef(200, 'matter')
 M500c = ccl.halos.MassDef(500, 'critical')
 M500m = ccl.halos.MassDef(500, 'matter')
 MDFS = [MVIR, MVIR, MVIR, MVIR,
@@ -66,7 +68,7 @@ def test_nM_watson_extra():
         assert np.all(np.isfinite(n))
         assert np.shape(n) == np.shape(m)
     for m in MS:
-        n = nM.get_mass_function(COSMO, m, 0.01)
+        n = nM.get_mass_function(COSMO, m, 0.1)
         assert np.all(np.isfinite(n))
         assert np.shape(n) == np.shape(m)
 
@@ -78,13 +80,14 @@ def test_nM_bocquet_extra(with_hydro):
                                     M500m,
                                     hydro=with_hydro)
 
-    nM = ccl.halos.MassFuncBocquet16(COSMO,
-                                     M500c,
-                                     hydro=with_hydro)
-    for m in MS:
-        n = nM.get_mass_function(COSMO, m, 0.9)
-        assert np.all(np.isfinite(n))
-        assert np.shape(n) == np.shape(m)
+    for md in [M500c, M200c, M200m]:
+        nM = ccl.halos.MassFuncBocquet16(COSMO,
+                                         M500c,
+                                         hydro=with_hydro)
+        for m in MS:
+            n = nM.get_mass_function(COSMO, m, 0.9)
+            assert np.all(np.isfinite(n))
+            assert np.shape(n) == np.shape(m)
 
 
 @pytest.mark.parametrize('name', ['Press74', 'Tinker08',
