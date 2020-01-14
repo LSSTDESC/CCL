66f58fdf25a81d0b03c03d02ba33c788b2b8a96b
elisachisari <n.e.chisari@uu.nl>
2020-01-14 17:16:48 +0100

    Remove blank below fnct def

--- pyccl/pyutils.py
+++ pyccl/pyutils.py
@@ -289,10 +289,9 @@ def _vectorize_fn4(fn, fn_vec, cosmo, x, a, d, returns_status=True):
     # Check result and return
     check(status, cosmo_in)
     return f
-    
+
 
 def _vectorize_fn5(fn, fn_vec, cosmo, x1, x2, returns_status=True):
-
     """Generic wrapper to allow vectorized (1D array) access to CCL
     functions with two vector arguments of the same length,
     with a cosmology dependence.
