c50116a1ec5d41e2c425a3392f2ea15093bf6973
Mustapha Ishak <mxi054000@utdallas.edu>
2020-03-31 15:14:30 -0500

    addressing travis compilation issue

--- .travis/install_isitgr_linux.sh
+++ .travis/install_isitgr_linux.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 git clone https://github.com/mishakb/ISiTGR
-cd ISiTGR/camb/fortran
-make
-
-# at this point the make file leaves you in the fortran dir
-cd Release
+cd ISiTGR/camb
+python setup.py install
