a766487120d966f1d3bbb4793f51bd817b0839ad
David Alonso <dam.phys@gmail.com>
2020-12-04 23:16:00 +0000

    class...

--- .github/scripts/install_class_macos.sh
+++ .github/scripts/install_class_macos.sh
@@ -2,6 +2,6 @@
 
 git clone https://github.com/lesgourg/class_public.git
 cd class_public
-sed -i 's/CC       = gcc/CC       = gcc-9/g' Makefile
+sed -i.bak -e 's/CC       = gcc/CC       = gcc-9/g' Makefile
 git checkout v2.7.2
 make
