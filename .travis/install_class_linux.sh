766ca033e86a042ee7a79b78a60204e75b351883
David Alonso <dam.phys@gmail.com>
2020-12-08 14:43:53 +0000

    Github actions (#836)
    * testing gh
    
    * test install
    
    * basic
    
    * almost there
    
    * fake
    
    * test name
    
    * coveralls
    
    * note
    
    * note
    
    * OSs and PYs
    
    * OSs and PYs
    
    * OSs and PYs
    
    * no mac for now
    
    * just mac, testing
    
    * just mac, testing
    
    * just mac, testing
    
    * just mac, testing
    
    * just mac, testing
    
    * just mac, testing
    
    * just mac, testing
    
    * just mac, testing
    
    * just mac, testing
    
    * just mac, testing
    
    * just mac, testing
    
    * just mac, testing
    
    * just mac, testing
    
    * mac and ubuntu
    
    * mac and ubuntu
    
    * mac and ubuntu
    
    * mac and ubuntu
    
    * mac and ubuntu
    
    * mac and ubuntu
    
    * mac and ubuntu
    
    * mac and ubuntu
    
    * mac and ubuntu
    
    * mac and ubuntu
    
    * class...
    
    * class...
    
    * class...
    
    * class...
    
    * class...
    
    * class...
    
    * class...
    
    * swig...
    
    * cleanup
    
    * oops
    
    * simpler class
    
    * c tests
    
    * c tests
    
    * MJ comments
    
    * master

--- .travis/install_class_linux.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/lesgourg/class_public.git
-cd class_public
-git checkout v2.7.2
-make
-
-# at this point the make file leaves you in the python dir
-cd ..
