751ab75be5a16430ab7628693c221de5cb4005f2
David Alonso <dam.phys@gmail.com>
2020-12-24 09:19:01 +0000

    typo

--- benchmarks/test_timing.py
+++ benchmarks/test_timing.py
@@ -10,7 +10,7 @@ def test_timing():
                     1.607983, 1.689579, 1.772899,
                     1.857700, 1.943754, 2.030887,
                     2.118943])
-    dNdz_file = np.load('data/dNdzs.npz')
+    dNdz_file = np.load('benchmarks/data/dNdzs.npz')
     z_s = dNdz_file['z_sh']
     dNdz_s = dNdz_file['dNdz_sh'].T
     z_g = dNdz_file['z_cl']
@@ -32,7 +32,6 @@ def test_timing():
     cls = np.zeros([nx, nl])
     ind1, ind2 = np.triu_indices(nt)
     for ix, (i1, i2) in enumerate(zip(ind1, ind2)):
-        print(i1, i2)
         cls[ix, :] = ccl.angular_cl(cosmo, t_all[i1], t_all[i2], ls)
     end = time.time()
     t_seconds = end - start
