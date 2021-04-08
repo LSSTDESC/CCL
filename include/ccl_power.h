f23df67da9fe23ae2b7dae3c788ef4beaa0a182f
David Alonso <dam.phys@gmail.com>
2021-04-08 19:39:45 +0100

    covariance done and tested, missing benchmarks

--- include/ccl_power.h
+++ include/ccl_power.h
@@ -18,6 +18,26 @@ void ccl_rescale_linpower(ccl_cosmology* cosmo, ccl_f2d_t *psp,
                           int *status);
 
 /**
+ * Variance of the projected matter density field with 2D (top-hat) smoothing scale R [Mpc].
+ * Returns sigma2(R) for specified cosmology at a = 1.
+ * @param cosmo Cosmology parameters and configurations
+ * @param R Smoothing scale, in [Mpc] units
+ * @param a scale factor
+ * @param psp input power spectrum.
+ * @param status Status flag. 0 if there are no errors, nonzero otherwise.
+ * For specific cases see documentation for ccl_error.c
+ * @return sigma(R).
+ */
+double ccl_sigma2B(ccl_cosmology *cosmo,double R,double a,
+                   ccl_f2d_t *psp, int *status);
+
+/**
+ * As `ccl_sigma2B`, calculated for an array of scale factors and smoothing scales.
+ */
+void ccl_sigma2Bs(ccl_cosmology *cosmo,int na, double *a, double *R,
+                  double *sigma2B_out, ccl_f2d_t *psp, int *status);
+
+/**
  * Variance of the matter density field with (top-hat) smoothing scale R [Mpc].
  * Returns sigma(R) for specified cosmology at a = 1.
  * @param cosmo Cosmology parameters and configurations
