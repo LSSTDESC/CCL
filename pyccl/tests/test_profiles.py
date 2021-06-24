ae8739a3b06a9ca4a53f1f5030ffd6b5012233d3
Nick Koukoufilippas <nikfilippas@gmail.com>
2021-06-24 15:31:18 +0300

    fixed test

--- pyccl/tests/test_profiles.py
+++ pyccl/tests/test_profiles.py
@@ -169,19 +169,24 @@ def test_hod_ns_independent(real_prof):
 def test_hod_2pt_raises():
     pbad = ccl.halos.HaloProfilePressureGNFW()
     c = ccl.halos.ConcentrationDuffy08(M200)
-    pgood = ccl.halos.HaloProfileHOD(c_M_relation=c)
-    pgood_b = ccl.halos.HaloProfileHOD(c_M_relation=c)
+    pgood = ccl.halos.HaloProfileHOD(c_M_relation=c, lM0_0=11.)
+    pgood_b = ccl.halos.HaloProfileHOD(c_M_relation=c, lM0_0=11.)
     p2 = ccl.halos.Profile2ptHOD()
     with pytest.raises(TypeError):
         p2.fourier_2pt(pbad, COSMO, 1., 1E13, 1.,
                        mass_def=M200)
 
-    with pytest.raises(ValueError):
-        p2.fourier_2pt(pgood, COSMO, 1., 1E13, 1.,
-                       prof2=pgood_b, mass_def=M200)
-
+    # doesn't raise because profiles are equivalent
     p2.fourier_2pt(pgood, COSMO, 1., 1E13, 1.,
                    prof2=pgood, mass_def=M200)
+
+    p2.fourier_2pt(pgood, COSMO, 1., 1E13, 1.,
+                   prof2=pgood_b, mass_def=M200)
+
+    with pytest.raises(ValueError):
+        pgood_b.update_parameters(lM0_0=10.)
+        p2.fourier_2pt(pgood, COSMO, 1., 1E13, 1.,
+                       prof2=pgood_b, mass_def=M200)
 
 
 def test_2pt_rcorr_smoke():
