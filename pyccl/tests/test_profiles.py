a272b6f14f28a59977ce5bf9d0cd20106b6ef08e
Nick Koukoufilippas <nikfilippas@gmail.com>
2021-07-04 19:27:16 +0300

    bugfix: no FFTLog key 'plaw_index'

--- pyccl/tests/test_profiles.py
+++ pyccl/tests/test_profiles.py
@@ -238,7 +238,7 @@ def test_projected_plaw_accuracy(alpha):
         return alpha
 
     p = ccl.halos.HaloProfilePowerLaw(r_scale=one_f, tilt=alpha_f)
-    p.update_precision_fftlog(plaw_index=alpha)
+    p.update_precision_fftlog(plaw_fourier=alpha)
 
     rt_arr = np.logspace(-3, 2, 1024)
     srt_arr = p.projected(COSMO, rt_arr, 1., 1.)
@@ -262,7 +262,7 @@ def test_plaw_accuracy(alpha):
         return alpha
 
     p = ccl.halos.HaloProfilePowerLaw(r_scale=one_f, tilt=alpha_f)
-    p.update_precision_fftlog(plaw_index=alpha)
+    p.update_precision_fftlog(plaw_fourier=alpha)
 
     k_arr = np.logspace(-3, 2, 1024)
     fk_arr = p.fourier(COSMO, k_arr, 1., 1.)
