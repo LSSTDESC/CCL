c35914f7e14a47e9f440e94649816ee1f733b5f9
Nick Koukoufilippas <nikfilippas@gmail.com>
2021-07-09 13:52:02 +0300

    fixed conflicts with master

--- pyccl/tests/test_cosmology.py
+++ pyccl/tests/test_cosmology.py
@@ -37,9 +37,9 @@ def test_cosmo_methods():
         cosmo.get_camb_pk_lin().eval(1., 1., cosmo)
     prof = ccl.halos.HaloProfilePressureGNFW()
     hmd = ccl.halos.MassDef200m()
-    hmf = ccl.halos.MassFuncTinker08(cosmo)
-    hbf = ccl.halos.HaloBiasTinker10(cosmo)
-    hmc = ccl.halos.HMCalculator(cosmo, massfunc=hmf, hbias=hbf, mass_def=hmd)
+    hmf = ccl.halos.MassFuncTinker08()
+    hbf = ccl.halos.HaloBiasTinker10()
+    hmc = ccl.halos.HMCalculator(massfunc=hmf, hbias=hbf, mass_def=hmd)
     assert ccl.halos.halomod_power_spectrum(cosmo, hmc, 1., 1., prof) == \
         cosmo.halomod_power_spectrum(hmc, 1., 1., prof)
 
