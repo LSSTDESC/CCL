d5ee4a24f2c866f8b12473e91f39f2db5468aed7
Nick Koukoufilippas <nikfilippas@gmail.com>
2021-07-14 18:02:27 +0300

    load sigma_critical in init

--- pyccl/__init__.py
+++ pyccl/__init__.py
@@ -22,7 +22,7 @@ from .core import Cosmology, CosmologyVanillaLCDM, CosmologyCalculator
 from .background import growth_factor, growth_factor_unnorm, \
     growth_rate, comoving_radial_distance, angular_diameter_distance, comoving_angular_distance, \
     h_over_h0, luminosity_distance, distance_modulus, scale_factor_of_chi, \
-    omega_x, rho_x
+    omega_x, rho_x, sigma_critical
 
 # Boltzmann solvers
 from .boltzmann import get_camb_pk_lin, get_isitgr_pk_lin, get_class_pk_lin
