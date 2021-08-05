3e77e18dab47b8a724451937940814a5423468b3
Nick Koukoufilippas <nikfilippas@gmail.com>
2021-08-05 11:49:00 +0300

    CCLWarning for deprecated modules

--- pyccl/__init__.py
+++ pyccl/__init__.py
@@ -20,8 +20,10 @@ from . import baryons, cells
 def __getattr__(name):
     rename = {"bcm": "baryons", "cls": "cells"}
     if name in rename:
+        from .errors import CCLWarning
         import warnings
-        warnings.warn(f"Module {name} has been renamed to {rename[name]}.")
+        warnings.warn(f"Module {name} has been renamed to {rename[name]}.",
+                      CCLWarning)
         name = rename[name]
         return eval(name)
     raise AttributeError(f"No module named {name}.")
