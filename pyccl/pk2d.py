d98b2ebca67770bb1e83c73149708c6a80b082dc
Nick Koukoufilippas <nikfilippas@gmail.com>
2021-11-15 10:23:50 +0200

    np.array_equiv instead of np.allclose

--- pyccl/pk2d.py
+++ pyccl/pk2d.py
@@ -342,9 +342,9 @@ class Pk2D(object):
         a_arr_a, lk_arr_a, pk_arr_a = self.get_spline_arrays()
         a_arr_b, lk_arr_b, pk_arr_b = other.get_spline_arrays()
 
-        same_a = np.allclose(a_arr_a, a_arr_b)
-        same_lk = np.allclose(lk_arr_a, lk_arr_b)
-        same_pk = np.allclose(pk_arr_a, pk_arr_b)
+        same_a = np.array_equiv(a_arr_a, a_arr_b)
+        same_lk = np.array_equiv(lk_arr_a, lk_arr_b)
+        same_pk = np.array_equiv(pk_arr_a, pk_arr_b)
         return same_a and same_lk and same_pk
 
     def __add__(self, other):
