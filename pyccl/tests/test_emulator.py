2e6cbe081c91e4fbe11ba13a1fbc276a73f992cc
Nick Koukoufilippas <nikfilippas@gmail.com>
2021-11-19 12:18:39 +0200

    testing Emulator base explicitly

---
+++ pyccl/tests/test_emulator.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pytest
+import pyccl as ccl
+from pyccl.emulator import Bounds
+from pyccl.pyutils import CCLWarning
+from pyccl.boltzmann import PowerSpectrumArico21
+
+def test_bounds_raises_warns():
+    bounds = {"a": [0, 1], "b": [1, 0]}
+    with pytest.raises(ValueError):
+        Bounds(bounds)
+
+    bounds = {"a": [0, 1], "b": [0, 1]}
+    proposal = {"a": 0, "b": -1}
+    B = Bounds(bounds)
+    with pytest.raises(ValueError):
+        B.check_bounds(proposal)
+
+    proposal = {"a": 0, "b": 0.5, "c": 1}
+    with pytest.warns(CCLWarning):
+        B.check_bounds(proposal)
+
+
+def test_emulator_load_raises():
+    with pytest.raises(NotImplementedError):
+        ccl.Emulator()
+
+
+def test_emulator_from_name_raises():
+    with pytest.raises(ValueError):
+        ccl.PowerSpectrumEmulator.from_name("hello_world")
