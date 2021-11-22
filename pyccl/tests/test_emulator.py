8afe3bbe13061cd5e49a0b5f6db8bee5a7d4fc71
Nick Koukoufilippas <nikfilippas@gmail.com>
2021-11-22 19:09:53 +0200

    testing every pk_emu method

--- pyccl/tests/test_emulator.py
+++ pyccl/tests/test_emulator.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 import pyccl as ccl
 from pyccl.emulator import Bounds
@@ -39,3 +40,26 @@ def test_arico21_raises():
             # ignore Tensorflow-related warnings
             warnings.simplefilter("ignore")
             cosmo.compute_nonlin_power()
+
+
+def test_arico21_linear_nonlin_equiv():
+    knl = np.geomspace(0.1, 5, 64)
+    extras = {"arico21": {'M_c': 14, 'eta': -0.3, 'beta': -0.22,
+                          'M1_z0_cen': 10.5, 'theta_out': 0.25,
+                          'theta_inn': -0.86, 'M_inn': 13.4}
+              }
+    cosmo = ccl.CosmologyVanillaLCDM(matter_power_spectrum="arico21",
+                                     baryons_power_spectrum="arico21",
+                                     extra_parameters=extras)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        cosmo.compute_nonlin_power()
+    pk0 = cosmo.get_nonlin_power().eval(knl, 1, cosmo)
+
+    pk1 = ccl.PowerSpectrumEmulator.get_pk_nonlin(cosmo, "arico21")
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        # NL + bar
+        pk1 = cosmo.baryon_correct("arico21", pk1).eval(knl, 1, cosmo)
+
+    assert np.allclose(pk0, pk1, rtol=5e-3)
