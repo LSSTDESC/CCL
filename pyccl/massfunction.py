b2c29ae6409bab732f9f8568a5a4f7c7998a047b
Nick Koukoufilippas <nikfilippas@gmail.com>
2022-01-21 19:47:26 +0200

    patch to replace new default None

--- pyccl/massfunction.py
+++ pyccl/massfunction.py
@@ -33,9 +33,7 @@ def massfunc(cosmo, halo_mass, a, overdensity=200):
     elif mf_par == 'angulo':
         mf = hal.MassFuncAngulo12(cosmo)
 
-    return mf.get_mass_function(cosmo,
-                                halo_mass,
-                                a)
+    return mf.get_mass_function(cosmo, halo_mass, a)
 
 
 @deprecated(hal.HaloBias)
@@ -56,6 +54,7 @@ def halo_bias(cosmo, halo_mass, a, overdensity=200):
     """
     mdef = hal.MassDef(overdensity, 'matter')
     mf_par = cosmo._config_init_kwargs['mass_function']
+    if mf_par is None: mf_par = 'tinker10'  # patch for None default
     if mf_par == 'tinker10':
         bf = hal.HaloBiasTinker10(cosmo, mass_def=mdef)
     elif mf_par == 'shethtormen':
