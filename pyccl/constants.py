5bbb186a0fea16de6ab97c9e7bf151dc8715fd3b
Nick Koukoufilippas <nikfilippas@gmail.com>
2022-02-07 23:14:46 +0200

    added caching switches and tests

--- pyccl/constants.py
+++ pyccl/constants.py
@@ -17,18 +17,21 @@ class Caching(object):
         _caches (``collections.OrderedDict``):
             Ordered dictionary of cached objects. The keys are formatted as
             ``{func_name}{config_hash}``.
-        maxsize (int):
+        maxsize (``int``):
             Maximum number of caches. If the dictionary is full, new caches
             are assigned in a rolling fashion (i.e. delete the oldest).
+        _enabled (``bool``):
+            To cache or not to cache.
     """
     _caches = OrderedDict()
+    _enabled = True
     maxsize = 64
 
     def __init__(self):
         pass
 
     @classmethod
-    def first(dic):
+    def first(cls, dic):
         """Get first element of ``OrderedDict``."""
         return next(iter(dic))
 
@@ -37,16 +40,19 @@ class Caching(object):
         """Decorator used to cache slow parts of the code."""
 
         def wrapper(instance):
+            if not cls._enabled:
+                return func(instance)
+
             key = func.__qualname__ + str(hash(instance))
 
             if key in cls._caches:
                 # output object has been cached
                 return cls._caches[key]
-            elif len(cls._caches) <= cls.maxsize:
+            elif len(cls._caches) < cls.maxsize:
                 # output object is not cached and there is space
                 out = func(instance)
                 cls._caches[key] = out
-            elif len(cls._caches) > cls.maxsize:
+            elif len(cls._caches) >= cls.maxsize:
                 # output object is not cached and there is no space
                 first_item = cls.first(cls._caches)
                 cls._caches.pop(first_item)
@@ -55,3 +61,16 @@ class Caching(object):
 
             return out
         return wrapper
+
+    @classmethod
+    def enable(cls, maxsize=64):
+        cls._enabled = True
+        cls.maxsize = maxsize
+
+    @classmethod
+    def disable(cls):
+        cls._enabled = False
+
+    @classmethod
+    def toggle(cls):
+        cls._enabled = not cls._enabled
