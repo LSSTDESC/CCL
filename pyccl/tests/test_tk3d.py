9d37a44e8769e18ac3d1d2278254eac201e1567e
Nick Koukoufilippas <nikfilippas@gmail.com>
2022-03-25 01:38:49 +0200

    first commit

--- pyccl/tests/test_tk3d.py
+++ pyccl/tests/test_tk3d.py
@@ -137,3 +137,23 @@ def test_tk3d_eval(is_product):
     ptrue = tkkaf(ktest[None, :], ktest[:, None], atest)
     phere = tsp.eval(ktest, atest)
     assert_allclose(phere.flatten(), ptrue.flatten(), rtol=1E-6)
+
+
+@pytest.mark.parametrize('is_product', [True, False])
+def test_tk3d_spline_arrays(is_product):
+    (a_arr, lk_arr, fka1_arr, fka2_arr, tkka_arr) = get_arrays()
+    if is_product:
+        tsp = ccl.Tk3D(a_arr, lk_arr, pk1_arr=fka1_arr, pk2_arr=fka2_arr)
+    else:
+        tsp = ccl.Tk3D(a_arr, lk_arr, tkk_arr=tkka_arr)
+
+    a_get, lk_get1, lk_get2, out = tsp.get_spline_arrays()
+    assert np.all(a_get == a_arr)
+    assert np.all(lk_get1 == lk_arr)
+    assert np.all(lk_get2 == lk_arr)
+
+    if is_product:
+        assert np.all(np.log(out[0]) == fka1_arr)
+        assert np.all(np.log(out[1]) == fka2_arr)
+    else:
+        assert np.all(np.log(out[0]) == tkka_arr)
