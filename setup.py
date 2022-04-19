69c153ebe7878818469395e0a64ae8d9ff648438
Tilman Troester <tilman.troester@gmail.com>
2022-04-19 13:04:10 +0100

    Allow debug flag builds from python. Add python setup.py clean.

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from setuptools.command.build_py import build_py as _build
 from setuptools.command.develop import develop as _develop
+from distutils.command.clean import clean as _clean
+from setuptools import Distribution as _distribution
 from setuptools import setup, find_packages
 from subprocess import call
 from io import open
@@ -8,12 +10,14 @@ import os
 import sys
 
 
-def _compile_ccl():
+def _compile_ccl(debug=False):
     call(["mkdir", "-p", "build"])
     v = sys.version_info
-    if call(["cmake", "-H.", "-Bbuild",
-             "-DPYTHON_VERSION=%d.%d.%d" % (
-                v.major, v.minor, v.micro)]) != 0:
+    cmd = ["cmake", "-H.", "-Bbuild",
+           "-DPYTHON_VERSION=%d.%d.%d" % (v.major, v.minor, v.micro)]
+    if debug:
+        cmd += ["-DCMAKE_BUILD_TYPE=Debug"]
+    if call(cmd) != 0:
         raise Exception(
             "Could not run CMake configuration. Make sure "
             "CMake is installed !")
@@ -32,18 +36,40 @@ def _compile_ccl():
                         "SWIG must have failed.")
 
 
-class build(_build):
+class Distribution(_distribution):
+    global_options = _distribution.global_options
+
+    global_options += [
+        ("debug", None, "Debug build"),
+    ]
+
+    def __init__(self, attr=None):
+        self.debug = False
+        super().__init__(attr)
+
+
+class Build(_build):
     """Specialized Python source builder."""
+
     def run(self):
-        _compile_ccl()
+        _compile_ccl(debug=self.distribution.debug)
         _build.run(self)
 
 
-class develop(_develop):
+class Develop(_develop):
     """Specialized Python develop mode."""
     def run(self):
-        _compile_ccl()
+        _compile_ccl(debug=self.distribution.debug)
         _develop.run(self)
+
+
+class Clean(_clean):
+    """Remove the copied _ccllib.so"""
+    def run(self):
+        if os.path.isfile("pyccl/_ccllib.so"):
+            os.remove("pyccl/_ccllib.so")
+            print("Removed pyccl/_ccllib.so")
+        _clean.run(self)
 
 
 # read the contents of the README file
@@ -62,9 +88,10 @@ setup(
     package_data={'pyccl': ['_ccllib.so']},
     include_package_data=True,
     use_scm_version=True,
+    distclass=Distribution,
     setup_requires=['setuptools_scm'],
     install_requires=['numpy', 'pyyaml'],
-    cmdclass={'build_py': build, 'develop': develop},
+    cmdclass={'build_py': Build, 'develop': Develop, 'clean': Clean},
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
