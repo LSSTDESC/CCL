625450ae2168bbbc4cc3881080765a118fbd0ce5
Matthew R. Becker <becker.mr@gmail.com>
2022-04-21 14:07:56 -0500

    TST try again to make it fail

--- pyccl/tests/test_halofit_highz.py
+++ pyccl/tests/test_halofit_highz.py
@@ -1,24 +1,28 @@
 import pyccl as ccl
-
+import pytest
 
 COSMO = ccl.Cosmology(
     Omega_c=0.27, Omega_b=0.045, h=0.67, sigma8=0.8, n_s=0.96,
     transfer_function='bbks', matter_power_spectrum='halofit')
 
 
-def test_halofit_highz():
-    a0 = 1.0/(1 + 25)
-    a1 = 1.0/(1 + 50)
+@pytest.mark.parametrize("zl,zh", [
+    (0, 1),
+    (25, 75),
+])
+def test_halofit_highz(zl, zh):
+    al = 1.0/(1 + zl)
+    ah = 1.0/(1 + zh)
 
     k = 10
-    pkrat0 = (
-        ccl.nonlin_matter_power(COSMO, k, a0)
-        / ccl.linear_matter_power(COSMO, k, a0)
+    pkratl = (
+        ccl.nonlin_matter_power(COSMO, k, al)
+        / ccl.linear_matter_power(COSMO, k, al)
     )
-    pkrat1 = (
-        ccl.nonlin_matter_power(COSMO, k, a1)
-        / ccl.linear_matter_power(COSMO, k, a1)
+    pkrath = (
+        ccl.nonlin_matter_power(COSMO, k, ah)
+        / ccl.linear_matter_power(COSMO, k, ah)
     )
 
-    print(pkrat0, pkrat1, flush=True)
-    assert pkrat0 > pkrat1
+    print(pkratl, pkrath, flush=True)
+    assert pkratl > pkrath
