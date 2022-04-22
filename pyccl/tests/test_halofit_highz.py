a0651909622902ddc57048b950ea1f0981b75c04
Matthew R. Becker <becker.mr@gmail.com>
2022-04-21 19:02:34 -0500

    TST test a bigger range of values

--- pyccl/tests/test_halofit_highz.py
+++ pyccl/tests/test_halofit_highz.py
@@ -1,28 +1,25 @@
 import numpy as np
 import pyccl as ccl
-import pytest
 
 COSMO = ccl.Cosmology(
     Omega_c=0.27, Omega_b=0.045, h=0.67, sigma8=0.8, n_s=0.96,
     transfer_function='bbks', matter_power_spectrum='halofit')
 
 
-@pytest.mark.parametrize("zl,zh", [
-    (0, 1),
-    (25, 75),
-])
-def test_halofit_highz(zl, zh):
-    al = 1.0/(1 + zl)
-    ah = 1.0/(1 + zh)
+def test_halofit_highz():
+    vals = [(25, 75)] + list(zip(range(0, 90), range(1, 99)))
+    for zl, zh in vals:
+        al = 1.0/(1 + zl)
+        ah = 1.0/(1 + zh)
 
-    k = np.logspace(0, 2, 10)
-    pkratl = (
-        ccl.nonlin_matter_power(COSMO, k, al)
-        / ccl.linear_matter_power(COSMO, k, al)
-    )
-    pkrath = (
-        ccl.nonlin_matter_power(COSMO, k, ah)
-        / ccl.linear_matter_power(COSMO, k, ah)
-    )
+        k = np.logspace(0, 2, 10)
+        pkratl = (
+            ccl.nonlin_matter_power(COSMO, k, al)
+            / ccl.linear_matter_power(COSMO, k, al)
+        )
+        pkrath = (
+            ccl.nonlin_matter_power(COSMO, k, ah)
+            / ccl.linear_matter_power(COSMO, k, ah)
+        )
 
-    assert np.all(pkratl >= pkrath), (pkratl, pkrath)
+        assert np.all(pkratl >= pkrath), (zl, zh, pkratl, pkrath)
