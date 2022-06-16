68933124af079d84858a3827e00c8b3532e0f686
Hsin Fan <57552401+hsinfan1996@users.noreply.github.com>
2022-06-17 00:55:08 +0800

    Add docstrings on shape of array_like a_source

--- pyccl/halos/profiles.py
+++ pyccl/halos/profiles.py
@@ -292,6 +292,7 @@ class HaloProfile(object):
             M (float or array_like): halo mass in units of M_sun.
             a_lens (float): scale factor of lens.
             a_source (float or array_like): scale factor of source.
+                If array_like, it must have the same shape as r.
             mass_def (:class:`~pyccl.halos.massdef.MassDef`):
                 a mass definition object.
 
@@ -325,6 +326,7 @@ class HaloProfile(object):
             M (float or array_like): halo mass in units of M_sun.
             a_lens (float): scale factor of lens.
             a_source (float or array_like): source's scale factor.
+                If array_like, it must have the same shape as r.
             mass_def (:class:`~pyccl.halos.massdef.MassDef`):
                 a mass definition object.
 
@@ -357,6 +359,7 @@ class HaloProfile(object):
             M (float or array_like): halo mass in units of M_sun.
             a_lens (float): scale factor of lens.
             a_source (float or array_like): source's scale factor.
+                If array_like, it must have the same shape as r.
             mass_def (:class:`~pyccl.halos.massdef.MassDef`):
                 a mass definition object.
 
@@ -384,6 +387,7 @@ class HaloProfile(object):
             M (float or array_like): halo mass in units of M_sun.
             a_lens (float): scale factor of lens.
             a_source (float or array_like): source's scale factor.
+                If array_like, it must have the same shape as r.
             mass_def (:class:`~pyccl.halos.massdef.MassDef`):
                 a mass definition object.
 
