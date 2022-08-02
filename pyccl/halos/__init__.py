91096038eee77641351e676b40a54908b8d26a7d
Christos Georgiou <chr.p.georgiou@gmail.com>
2022-08-02 23:32:33 +0200

    Added SatelliteShearHOD class to __all__.

--- pyccl/halos/__init__.py
+++ pyccl/halos/__init__.py
@@ -101,7 +101,7 @@ __all__ = (
     'HaloBiasBhattacharya11', 'halo_bias_from_name',
     'HaloProfile', 'HaloProfileGaussian', 'HaloProfilePowerLaw',
     'HaloProfileNFW', 'HaloProfileEinasto', 'HaloProfileHernquist',
-    'HaloProfilePressureGNFW', 'HaloProfileHOD',
+    'HaloProfilePressureGNFW', 'HaloProfileHOD', 'SatelliteShearHOD',
     'Profile2pt', 'Profile2ptHOD',
     'HMCalculator', 'halomod_mean_profile_1pt', 'halomod_bias_1pt',
     'halomod_power_spectrum', 'halomod_Pk2D', 'halomod_trispectrum_1h',
