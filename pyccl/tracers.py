d6f3455a3ff5f383e01e26652e83085d3cc2a217
David Alonso <dam.phys@gmail.com>
2022-08-05 17:02:50 +0100

    fix 823

--- pyccl/tracers.py
+++ pyccl/tracers.py
@@ -555,6 +555,11 @@ class Tracer(object):
             ta_s = NoneArr
             tk_s = NoneArr
 
+        if a_s is not None:
+            if not np.all(np.diff(a_s) > 0):
+                raise ValueError("Scale factor must be monotonically "
+                                 "increasing")
+
         status = 0
         ret = lib.cl_tracer_t_new_wrapper(cosmo.cosmo,
                                           int(der_bessel),
