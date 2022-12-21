029f3361688d448e21fc9c8da196deb924e0a8c8
Hsin Fan <57552401+hsinfan1996@users.noreply.github.com>
2022-12-22 01:00:10 +0800

    Minor fixes

--- pyccl/__init__.py
+++ pyccl/__init__.py
@@ -19,8 +19,10 @@ from . import ccllib as lib
 # monkey patch for isitgr and fast-pt if Numpy>=1.24
 from packaging.version import parse
 import numpy as np
-if bool(parse(np.__version__) >= parse('1.24')):
+if (parse(np.__version__) >= parse('1.24')):
     np.int = int
+del parse
+del np
 
 # Errors
 from .errors import (
