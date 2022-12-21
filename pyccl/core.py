7c8019f53ac7883336888ddd02c6440740ffb349
Hsin Fan <57552401+hsinfan1996@users.noreply.github.com>
2022-12-21 19:46:17 +0800

    Fix for build-in type

--- pyccl/core.py
+++ pyccl/core.py
@@ -280,9 +280,9 @@ class Cosmology(object):
         """
         def make_yaml_friendly(d):
             for k, v in d.items():
-                if isinstance(v, np.floating):
+                if isinstance(v, (float, np.floating)):
                     d[k] = float(v)
-                elif isinstance(v, np.integer):
+                elif isinstance(v, (int, np.integer)):
                     d[k] = int(v)
                 elif isinstance(v, bool):
                     d[k] = bool(v)
