1626b787fa1544664a478ae45e932ceb43bc0052
David Alonso <dam.phys@gmail.com>
2023-03-20 19:24:02 +0000

    remove from C layer

--- pyccl/bcm.py
+++ pyccl/bcm.py
@@ -26,10 +26,16 @@ def bcm_model_fka(cosmo, k, a):
         float or array_like: Correction factor to apply to the power spectrum.
     """
     k_use = np.atleast_1d(k)
-    status = 0
-    fka, status = lib.bcm_model_fka_vec(cosmo.cosmo, a, k_use,
-                                        len(k_use), status)
-    check(status, cosmo)
+
+    z = 1./a - 1.
+    kh = k_use / cosmo['h']
+    b0 = 0.105*cosmo['bcm_log10Mc'] - 1.27
+    bfunc = b0 / (1. + (z/2.3)**2.5)
+    bfunc4 = (1-bfunc)**4
+    kg = 0.7 * bfunc4 * cosmo['bcm_etab']**(-1.6)
+    gf = bfunc / (1 + (kh/kg)**3) + 1. - bfunc  # k in h/Mpc
+    scomp = 1 + (kh / cosmo['bcm_ks'])**2  # k in h/Mpc
+    fka = gf * scomp
 
     if np.ndim(k) == 0:
         fka = fka[0]
@@ -44,8 +50,23 @@ def bcm_correct_pk2d(cosmo, pk2d):
         cosmo (:class:`~pyccl.core.Cosmology`): Cosmological parameters.
         pk2d (:class:`~pyccl.pk2d.Pk2D`): power spectrum.
     """
+
     if not isinstance(pk2d, Pk2D):
         raise TypeError("pk2d must be a Pk2D object")
+    a_arr, lk_arr, pk_arr = pk2d.get_spline_arrays()
+    k_arr = np.exp(lk_arr)
+    fka = np.array([bcm_model_fka(cosmo, k_arr, a) for a in a_arr])
+    pk_arr *= fka
+
+    logp = np.all(pk_arr > 0)
+    if logp:
+        pk_arr = np.log(pk_arr)
+
+    lib.f2d_t_free(pk2d.psp)
     status = 0
-    status = lib.bcm_correct(cosmo.cosmo, pk2d.psp, status)
+    pk2d.psp, status = lib.set_pk2d_new_from_arrays(
+        lk_arr, a_arr, pk_arr.flatten(),
+        int(pk2d.extrap_order_lok),
+        int(pk2d.extrap_order_hik),
+        int(logp), status)
     check(status, cosmo)
