6df4a05d0caf291485944de6549a8b032d9c84d1
David Alonso <dam.phys@gmail.com>
2023-05-16 16:02:11 +0100

    rtd fix bullets

--- readthedocs/conf.py
+++ readthedocs/conf.py
@@ -20,7 +20,6 @@ import os
 from os.path import abspath, dirname, join as pjoin
 import sys
 import sphinx_rtd_theme
-import importlib
 
 this_dir = dirname(abspath(__file__))
 root_path = abspath(pjoin(this_dir, '../'))
@@ -132,13 +131,8 @@ todo_include_todos = True
 # a list of builtin themes.
 #
 
-options = ["sphinx_rtd_theme", "sphinx_book_theme",]
-html_theme = options[1]
-html_theme_module = importlib.import_module(html_theme)
-try:
-    html_theme_path = [html_theme_module.get_html_theme_path()]
-except AttributeError:
-    pass
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
