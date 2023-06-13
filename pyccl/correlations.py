d9c57f16807d355f1981cb9a665440448853f3ea
Christos Georgiou <chr.p.georgiou@gmail.com>
2023-06-13 17:18:57 +0200

    Updated correlation_ab to v3.

--- pyccl/correlations.py
+++ pyccl/correlations.py
@@ -1,12 +1,12 @@
 __all__ = ("CorrelationMethods", "CorrelationTypes", "correlation",
            "correlation_3d", "correlation_multipole", "correlation_3dRsd",
-           "correlation_3dRsd_avgmu", "correlation_pi_sigma",)
+           "correlation_3dRsd_avgmu", "correlation_pi_sigma",
+           "correlation_ab")
 
 from enum import Enum
 import numpy as np
 from .pyutils import resample_array, _fftlog_transform
-from .parameters import physical_constants
-from . import DEFAULT_POWER_SPECTRUM, check, lib
+from . import DEFAULT_POWER_SPECTRUM, check, lib, physical_constants
 
 
 class CorrelationMethods(Enum):
@@ -354,12 +354,13 @@ def correlation_pi_sigma(cosmo, *, pi, sigma, a, beta,
     return xis
 
 
-def correlation_ab(cosmo, r_p: np.ndarray, z: np.ndarray,
+def correlation_ab(cosmo, *, r_p: np.ndarray, z: np.ndarray,
                    dndz: np.ndarray = None, dndz2: np.ndarray = None,
                    p_of_k_a=None,
                    type: str = 'gg',
                    precision_fftlog: dict = None):
-    """Computes :math:`w_{ab}`.
+    """
+    Computes :math:`w_{ab}`.
     .. math::
         \\w_{ab}(r_p)=\\int dz\\,(W)(z)
         \\int\\frac{dk k}{2\\pi^2}\\,P(k,a)\\,J_n(k r_p)
@@ -368,16 +369,18 @@ def correlation_ab(cosmo, r_p: np.ndarray, z: np.ndarray,
             cosmo (:class:`~pyccl.core.Cosmology`): A Cosmology object.
             r_p (float or array-like): Projected radial separation where the
                 correlation function will be evaluated.
-            z (array-like): Redshift values where the redshift distribution
-                has been evaluated. If float, the window function is set to
-                a delta function.
+            z (float or array-like): Redshift values where the redshift 
+                distribution has been evaluated. If float, the window function
+                is set to a delta function.
             dndz (array-like): Redshift distribution to be used when computing
                 the window function (see e.g. Eq. (3.11) in (1811.09598)).
             dndz2 (array-like or None): Redshift distribution corresponding to
                 sample `b` of tracers to be correlated. If None, tracers `a`
                 and `b` are the same. Default value: None.
-            p_of_k_a (:class:`~pyccl.pk2d.Pk2D`): 3D Power spectrum to
-                integrate.
+            p_of_k_a (:class:`~pyccl.pk2d.Pk2D`, :obj:`str` or :obj:`None`): 3D Power spectrum
+                to integrate. If a string, it must correspond to one of the
+                non-linear power spectra stored in `cosmo` (e.g.
+                `'delta_matter:delta_matter'`).
             type (string): Type of `ab` correlation. This changes the Bessel
                 function in the integral above. Choices: 'gg' (`J_0`),
                 'g+' (`J_2`), '++' (`J_0+J_4`).
@@ -393,9 +396,13 @@ def correlation_ab(cosmo, r_p: np.ndarray, z: np.ndarray,
         Returns:
             array-like: Value(s) of the correlation function at the input
             projected radial separation(s).
-    """
-    # TODO: add functionality for p_of_k_a=None.
+    """ # noqa
+    cosmo.compute_nonlin_power()
+    cosmo_in = cosmo
+    psp = cosmo_in.parse_pk(p_of_k_a)
     # TODO: add some unit tests for Value Errors.
+    # TODO: add the benchmark test.
+
     if dndz2 is None:
         dndz2 = dndz
     if dndz is None and np.iterable(z):
@@ -404,17 +411,17 @@ def correlation_ab(cosmo, r_p: np.ndarray, z: np.ndarray,
     a = 1/(1+z)
     a = np.atleast_1d(a)
     if type == 'gg':
-        xi = np.array([_fftlog_wrap(r_p, a_, cosmo, p_of_k_a, n=0,
+        xi = np.array([_fftlog_wrap(r_p, a_, psp, n=0,
                                     precision_fftlog=precision_fftlog)
                       for a_ in a])
     elif type == 'g+':
-        xi = np.array([_fftlog_wrap(r_p, a_, cosmo, p_of_k_a, n=2,
+        xi = np.array([_fftlog_wrap(r_p, a_, psp, n=2,
                                     precision_fftlog=precision_fftlog)
                       for a_ in a])
     elif type == '++':
-        xi = np.array([(_fftlog_wrap(r_p, a_, cosmo, p_of_k_a, n=0,
+        xi = np.array([(_fftlog_wrap(r_p, a_, psp, n=0,
                                      precision_fftlog=precision_fftlog) +
-                       _fftlog_wrap(r_p, a_, cosmo, p_of_k_a, n=4,
+                       _fftlog_wrap(r_p, a_, psp, n=4,
                                     precision_fftlog=precision_fftlog))
                       for a_ in a])
     else:
@@ -422,9 +429,11 @@ def correlation_ab(cosmo, r_p: np.ndarray, z: np.ndarray,
                          'values: "gg", "g+", "++".')
     if np.iterable(z):
         speed_of_light_kmps = physical_constants.CLIGHT * 1e-3
-        dchi_dz = speed_of_light_kmps/(cosmo.h_over_h0(a)*cosmo['H0'])
-        wz = dndz * dndz2 / (
-            cosmo.comoving_radial_distance(a) ** 2. * dchi_dz)
+        dchi_dz = speed_of_light_kmps/(cosmo_in.h_over_h0(a)*cosmo_in['H0'])
+        denominator = cosmo_in.comoving_radial_distance(a) ** 2. * dchi_dz
+        wz = np.divide(dndz * dndz2,
+                       denominator,
+                       where=denominator > 0)
         wz /= np.trapz(wz, z)
         if np.ndim(xi) == 2:
             wz = wz.reshape((len(z), 1))
@@ -434,9 +443,9 @@ def correlation_ab(cosmo, r_p: np.ndarray, z: np.ndarray,
     return w
 
 
-def _fftlog_wrap(r_p: np.ndarray, a: float, cosmo, p_of_k_a=None,
+def _fftlog_wrap(r_p: np.ndarray, a: float, p_of_k_a,
                  precision_fftlog: dict = None,
-                 n=0, large_padding=True):
+                 n=0):
     '''Computes the integral
        .. math::
           \\xi(r_p,a)=\\int\\frac{dk k}{2\\pi^2}\\,P(k,a)\\,J_n(k r_p)
@@ -470,7 +479,7 @@ def _fftlog_wrap(r_p: np.ndarray, a: float, cosmo, p_of_k_a=None,
     k_arr = np.geomspace(k_min, k_max, n_k)  # Array to be used by FFTLog.
 
     # What needs to go in _fftlog_transform to get out \xi_gg.
-    fk = p_of_k_a.eval(k_arr, a, cosmo)/((2*np.pi)**(5./2)*k_arr**(1/2))
+    fk = p_of_k_a(k_arr, a)/((2*np.pi)**(5./2)*k_arr**(1/2))
     r_arr, xi_fourier = _fftlog_transform(k_arr, fk, 3, l,
                                           precision_fftlog['plaw_fourier'])
     lr_arr = np.log(r_arr)
