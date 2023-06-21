2dd905bfb5db6ecd866a4f4f47163d27e2d7e22d
paulrogozenski <paulrogozenski@gmail.com>
2023-06-21 09:47:08 -0700

    added fftlog tests

--- pyccl/tests/test_fftlog.py
+++ pyccl/tests/test_fftlog.py
@@ -12,7 +12,7 @@ def fr(r, alpha, mu, dim):
 
     g1 = gamma(0.5 * (dim - alpha + mu))
     g2 = gamma(0.5 * (alpha + mu))
-    den = np.pi ** (dim / 2.0) * 2**alpha
+    den = np.pi ** (dim / 2.0) * 2**(alpha)
     return g1 / (g2 * den * r ** (dim - alpha))
 
 
@@ -30,10 +30,27 @@ def test_fftlog_plaw(dim, mu, alpha):
     fk_arr = fk(k_arr, alpha, mu, dim)
 
     r_arr, fr_arr = _fftlog_transform(k_arr, fk_arr, dim, mu, -alpha)
-    r_arr_2, fr_arr_2 = _fftlog_transform_general(
-        k_arr, fk_arr, mu, -alpha, 0, 0, 0
-    )
 
+    if dim == 2:
+        ccl_r, ccl_Fr = _fftlog_transform_general(k_arr,
+                                                  fk_arr * (k_arr**(dim)),
+                                                  mu + 1,
+                                                  -alpha + dim,
+                                                  0, 0.0, 0.0)
+        ccl_Fr /= 2 * np.pi * (dim + mu - alpha) 
+    elif dim == 3:
+        ccl_r, ccl_Fr = _fftlog_transform_general(k_arr,
+                                                  fk_arr * (k_arr**(dim)),
+                                                  mu,
+                                                  -alpha + dim,
+                                                  1, 0.0, 0.0)
+        ccl_Fr /= 2 * np.pi**2
+    else:
+        print('unsupported _fftlog_transform test!')
+        assert (0)
+    fr_arr_pred = fr(ccl_r, alpha, mu, dim)
+    res = np.fabs(ccl_Fr / fr_arr_pred - 1)
+    assert np.all(res < 1e-10)
     fr_arr_pred = fr(r_arr, alpha, mu, dim)
     res = np.fabs(fr_arr / fr_arr_pred - 1)
     assert np.all(res < 1e-10)
@@ -51,22 +68,29 @@ def test_fftlog_shapes():
     # Scalar rs
     with pytest.raises(ValueError):
         _fftlog_transform(k_arr[0], fk_arr, 2, 0, 1.5)
+    with pytest.raises(ValueError):
+        _fftlog_transform_general(k_arr[0], fk_arr, 0, 1.5, 0, 0.0, 0.0)
 
     # Scalar frs
     with pytest.raises(ValueError):
         _fftlog_transform(k_arr, fk_arr[0][0], 2, 0, 1.5)
+    with pytest.raises(ValueError):
+        _fftlog_transform_general(k_arr, fk_arr[0][0], 0, 1.5, 0, 0.0, 0.0)
 
     # Wrong rs
     with pytest.raises(ValueError):
         _fftlog_transform(k_arr[1:], fk_arr, 2, 0, 1.5)
+    with pytest.raises(ValueError):
+        _fftlog_transform_general(k_arr[1:], fk_arr, 0, 1.5, 0, 0.0, 0.0)
 
     # Single transform
     r, fr = _fftlog_transform(k_arr, fk_arr[0], 2, 0, 1.5)
+    assert fr.shape == (nk,)
+    r, fr = _fftlog_transform_general(k_arr, fk_arr[0], 0, 1.5, 0, 0.0, 0.0)
     assert fr.shape == (nk,)
 
     # Multiple transforms
     r, fr = _fftlog_transform(k_arr, fk_arr, 2, 0, 1.5)
     assert fr.shape == (nt, nk)
-
-
-test_fftlog_plaw(2, 0.5, 1.2)
+    r, fr = _fftlog_transform_general(k_arr, fk_arr, 0, 1.5, 0, 0.0, 0.0)
+    assert fr.shape == (nt, nk)
