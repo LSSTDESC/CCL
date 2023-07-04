76978db0cdcf221a79e98a9e949d70b6205e517f
Matteo Zennaro <matteo.zennaro90@gmail.com>
2023-07-04 17:04:47 +0100

    baccoemu baryons added to baryonic models

---
+++ pyccl/baryons/baccoemu_baryons.py
@@ -0,0 +1,136 @@
+__all__ = ("BaccoemuBaryons",)
+
+import numpy as np
+
+from .. import Pk2D
+from . import Baryons
+
+
+class BaccoemuBaryons(Baryons):
+    """See https://arxiv.org/abs/2011.15018 and https://bacco.dipc.org/emulator.html
+    """
+    name = 'BaccoemuBaryons'
+    __repr_attrs__ = __eq_attrs__ = ("log10_M_c", "log10_eta", "log10_beta", "log10_M1_z0_cen", 
+                                     "log10_theta_out", "log10_theta_inn", "log10_M_inn")
+    def __init__(self, log10_M_c=14, log10_eta=-0.3, log10_beta=-0.22, log10_M1_z0_cen=10.5, 
+                 log10_theta_out=0.25, log10_theta_inn=-0.86, log10_M_inn=13.4):
+        import warnings
+        with warnings.catch_warnings():
+            warnings.filterwarnings('ignore', category=UserWarning)
+            import baccoemu 
+            self.mpk = baccoemu.Matter_powerspectrum()
+            self.a_min = self.mpk.emulator['baryon']['bounds'][-1][0]
+            self.a_max = self.mpk.emulator['baryon']['bounds'][-1][1]
+            self.k_min = self.mpk.emulator['baryon']['k'][0]
+            self.k_max = self.mpk.emulator['baryon']['k'][-1]
+        
+        self.bcm_params = {
+            'M_c'           : log10_M_c,
+            'eta'           : log10_eta,
+            'beta'          : log10_beta,
+            'M1_z0_cen'     : log10_M1_z0_cen,
+            'theta_out'     : log10_theta_out,
+            'theta_inn'     : log10_theta_inn,
+            'M_inn'         : log10_M_inn
+        }
+    
+    def _sigma8tot_2_sigma8cold(self, emupars, sigma8tot):
+        if hasattr(emupars['omega_cold'], '__len__'):
+            _emupars = {}
+            for pname in emupars:
+                _emupars[pname] = emupars[pname][0]
+        else:
+            _emupars = emupars
+        A_s_fid = 2.1e-9
+        sigma8tot_fid = self.mpk.get_sigma8(cold=False, A_s=A_s_fid, **_emupars)
+        A_s = (sigma8tot / sigma8tot_fid)**2 * A_s_fid
+        return self.mpk.get_sigma8(cold=True, A_s=A_s, **_emupars)
+
+    def boost_factor(self, cosmo, k, a):
+        """The baccoemu BCM model boost factor for baryons.
+
+        Args:
+            cosmo (:class:`~pyccl.cosmology.Cosmology`): Cosmological parameters.
+            k (:obj:`float` or `array`): Wavenumber (in :math:`{\\rm Mpc}^{-1}`).
+            a (:obj:`float` or `array`): Scale factor.
+
+        Returns:
+            :obj:`float` or `array`: Correction factor to apply to
+                the power spectrum.
+        """ # noqa
+
+        if hasattr(a, '__len__'):
+            emupars = dict(
+                omega_cold = np.full((len(a)), cosmo['Omega_c'] + cosmo['Omega_b']),
+                omega_baryon = np.full((len(a)), cosmo['Omega_b']),
+                ns = np.full((len(a)), cosmo['n_s']),
+                hubble = np.full((len(a)), cosmo['h']),
+                neutrino_mass = np.full((len(a)), np.sum(cosmo['m_nu'])),
+                w0 = np.full((len(a)), cosmo['w0']),
+                wa = np.full((len(a)), cosmo['wa']),
+                expfactor = a
+            )
+        else:
+            emupars = dict(
+                omega_cold = cosmo['Omega_c'] + cosmo['Omega_b'],
+                omega_baryon = cosmo['Omega_b'],
+                ns = cosmo['n_s'],
+                hubble = cosmo['h'],
+                neutrino_mass = np.sum(cosmo['m_nu']),
+                w0 = cosmo['w0'],
+                wa = cosmo['wa'],
+                expfactor = a
+            )
+
+        if np.isnan(cosmo['A_s']):
+            sigma8tot = cosmo['sigma8']
+            sigma8cold = self._sigma8tot_2_sigma8cold(emupars, sigma8tot)
+            if hasattr(a, '__len__'):
+                emupars['sigma8_cold'] = np.full((len(a)), sigma8cold)
+            else:
+                emupars['sigma8_cold'] = sigma8cold
+        else:
+            if hasattr(a, '__len__'):
+                emupars['A_s'] = np.full((len(a)), cosmo['A_s'])
+            else:
+                emupars['A_s'] = cosmo['A_s']
+
+        _, fka = self.mpk.get_baryonic_boost(k=k / cosmo['h'], **{**emupars, **self.bcm_params})
+        
+        return fka
+
+    def update_parameters(self, log10_M_c=None, log10_eta=None, log10_beta=None, log10_M1_z0_cen=None, 
+                          log10_theta_out=None, log10_theta_inn=None, log10_M_inn=None):
+        """Update BCM parameters. All parameters set to ``None`` will
+        be left untouched.
+        """
+        if log10_M_c is not None:
+            self.bcm_params.update({'log10_M_c', log10_M_c})
+        if log10_eta is not None:
+            self.bcm_params.update({'log10_eta', log10_eta})
+        if log10_beta is not None:
+            self.bcm_params.update({'log10_beta', log10_beta})
+        if log10_M1_z0_cen is not None:
+            self.bcm_params.update({'log10_M1_z0_cen', log10_M1_z0_cen})
+        if log10_theta_out is not None:
+            self.bcm_params.update({'log10_theta_out', log10_theta_out})
+        if log10_theta_inn is not None:
+            self.bcm_params.update({'log10_theta_inn', log10_theta_inn})
+        if log10_M_inn is not None:
+            self.bcm_params.update({'log10_M_inn', log10_M_inn})
+        
+
+    def _include_baryonic_effects(self, cosmo, pk):
+        # Applies boost factor
+        a_arr, lk_arr, pk_arr = pk.get_spline_arrays()
+        k_arr = np.exp(lk_arr)
+        fka = self.boost_factor(cosmo, k_arr, a_arr)
+        pk_arr *= fka
+
+        if pk.psp.is_log:
+            np.log(pk_arr, out=pk_arr)  # in-place log
+
+        return Pk2D(a_arr=a_arr, lk_arr=lk_arr, pk_arr=pk_arr,
+                    is_logp=pk.psp.is_log,
+                    extrap_order_lok=pk.extrap_order_lok,
+                    extrap_order_hik=pk.extrap_order_hik)
