8457b90510e93aa67f5d10008eb9d02d44634a60
Matteo Zennaro <matteo.zennaro90@gmail.com>
2023-07-04 18:37:49 +0100

    Added benchmark tests for baccoemu linear and nonlinear emulator power spectra

---
+++ benchmarks/test_baccoemu_nonlinear.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pyccl as ccl
+
+BEMUNL_TOLERANCE = 5e-3
+
+
+def test_baccoemu_nonlinear():
+    bemu = ccl.BaccoemuNonlinear()
+    cosmo = ccl.Cosmology(
+        Omega_c=0.27,
+        Omega_b=0.05,
+        h=0.67,
+        sigma8=0.83,
+        n_s=0.96,
+        Neff=3.046,
+        mass_split='normal',
+        m_nu=0.1,
+        Omega_g=0,
+        Omega_k=0,
+        w0=-1,
+        wa=0,
+        matter_power_spectrum=bemu)
+
+    data = np.loadtxt("./benchmarks/data/baccoemu_nonlinear.txt")
+
+    k = data[:, 0] * cosmo['h']
+    pk = data[:, 1] / cosmo['h']**3
+    a = 1
+    
+    nlpk = cosmo.get_nonlin_power()
+    err = np.abs(pk / nlpk(k, a) - 1)
+    assert np.allclose(err, 0, atol=BEMUNL_TOLERANCE, rtol=0)
