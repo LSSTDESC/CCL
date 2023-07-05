664da0b3134eea2ae59e45c0ad81b85c793c6d28
Matteo Zennaro <matteo.zennaro90@gmail.com>
2023-07-05 18:45:58 +0100

    Added tests

--- benchmarks/test_baccoemu_linear.py
+++ benchmarks/test_baccoemu_linear.py
@@ -30,3 +30,47 @@ def test_baccoemu_linear():
     linpk = cosmo.get_linear_power()
     err = np.abs(pk / linpk(k, a) - 1)
     assert np.allclose(err, 0, atol=BEMULIN_TOLERANCE, rtol=0)
+
+    ktest, pktest = bemu.get_pk_at_a(1, cosmo)
+    pktest = np.exp(np.interp(np.log(k), np.log(ktest), np.log(pktest)))
+    err = np.abs(pktest / pk - 1)
+    assert np.allclose(err, 0, atol=BEMULIN_TOLERANCE, rtol=0)
+
+
+def test_baccoemu_linear_As_sigma8():
+    bemu = ccl.BaccoemuLinear()
+    cosmo1 = ccl.Cosmology(
+        Omega_c=0.27,
+        Omega_b=0.05,
+        h=0.67,
+        sigma8=0.83,
+        n_s=0.96,
+        Neff=3.046,
+        mass_split='normal',
+        m_nu=0.1,
+        Omega_g=0,
+        Omega_k=0,
+        w0=-1,
+        wa=0,
+        transfer_function=bemu)
+
+    cosmo2 = ccl.Cosmology(
+        Omega_c=0.27,
+        Omega_b=0.05,
+        h=0.67,
+        A_s=2.2194e-09,
+        n_s=0.96,
+        Neff=3.046,
+        mass_split='normal',
+        m_nu=0.1,
+        Omega_g=0,
+        Omega_k=0,
+        w0=-1,
+        wa=0,
+        transfer_function=bemu)
+
+    k1, pk1 = bemu.get_pk_at_a(1, cosmo1)
+    k2, pk2 = bemu.get_pk_at_a(1, cosmo2)
+
+    err = np.abs(pk1 / pk2 - 1)
+    assert np.allclose(err, 0, atol=BEMULIN_TOLERANCE, rtol=0)
