a6d51a5dbc59a0571197b2bae8b3bd8d3b130fb4
Matteo Zennaro <matteo.zennaro90@gmail.com>
2023-07-06 11:12:26 +0100

    Move baryon test from benchmarks to pyccl/tests

--- pyccl/tests/test_baccoemu.py
+++ pyccl/tests/test_baccoemu.py
@@ -1,11 +1,13 @@
+import pytest
 import numpy as np
 import pyccl as ccl
 
 BEMBAR_TOLERANCE = 1e-3
 
 
-def test_baccoemu_baryons():
+def test_baccoemu_baryons_boost():
     baryons = ccl.BaccoemuBaryons()
+    nlpkemu = ccl.BaccoemuNonlinear()
     cosmo = ccl.Cosmology(
         Omega_c=0.27,
         Omega_b=0.05,
@@ -18,15 +20,13 @@ def test_baccoemu_baryons():
         Omega_g=0,
         Omega_k=0,
         w0=-1,
-        wa=0)
+        wa=0,
+        matter_power_spectrum=nlpkemu)
 
-    data = np.loadtxt("./benchmarks/data/baccoemu_baryons_fk.txt")
-
-    k = data[:, 0] * cosmo['h']
-    fk = data[:, 1]
-    a = 1
-
-    cclfk = baryons.boost_factor(cosmo, k, a)
+    k = np.logspace(-2, 0.5, 100)
+    cclfk = baryons.boost_factor(cosmo, k, 1)
+    pk_gro = cosmo.get_nonlin_power()
+    pk_bcm = baryons.include_baryonic_effects(cosmo, pk_gro)
+    fk = pk_bcm(k, 1) / pk_gro(k, 1)
     err = np.abs(fk / cclfk - 1)
     assert np.allclose(err, 0, atol=BEMBAR_TOLERANCE, rtol=0)
-
