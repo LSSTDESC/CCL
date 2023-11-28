71eb1aaddc8d1592117d0d7b7188f2f6532b2aeb
CarterDW <140671500+CarterDW@users.noreply.github.com>
2023-11-28 10:13:47 -0600

    Update test_ept_power.py

--- pyccl/tests/test_ept_power.py
+++ pyccl/tests/test_ept_power.py
@@ -3,8 +3,6 @@ import pyccl as ccl
 import pytest
 from contextlib import nullcontext
 
-#Delete    
-#Delete
 COSMO = ccl.Cosmology(
     Omega_c=0.27, Omega_b=0.045, h=0.67, sigma8=0.8, n_s=0.96,
     transfer_function='bbks', matter_power_spectrum='linear')
