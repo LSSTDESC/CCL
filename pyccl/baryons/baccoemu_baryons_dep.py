fcd595a3c73ce6e11f4f51b7e2f2088c6864e835
zdu863 <zdu863@gmail.com>
2024-03-27 09:52:33 -0500

    fix flake8

--- pyccl/baryons/baccoemu_baryons_dep.py
+++ pyccl/baryons/baccoemu_baryons_dep.py
@@ -1,16 +1,17 @@
 __all__ = ("BaccoemuBaryons",)
 
-import numpy as np
-from copy import deepcopy
 from warnings import warn
 
-from .. import Pk2D
 from . import BaryonsBaccoemu
+
 
 class BaccoemuBaryons(BaryonsBaccoemu):
     name = 'BaccoemuBaryons'
+
     def __init__(self, *args, **kwargs):
         """This throws a deprecation warning on initialization."""
-        warn(f'Class {self.__class__.__name__} will be deprecated. Please use {BaryonsBaccoemu.__name__} instead.', DeprecationWarning, stacklevel=2)
+        warn(f'Class {self.__class__.__name__} will be deprecated. ' +
+             f'Please use {BaryonsBaccoemu.__name__} instead.',
+             DeprecationWarning, stacklevel=2)
         super().__init__(*args, **kwargs)
     pass
