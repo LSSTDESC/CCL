5819a15d400c3eefadc7bfedb9ddc897784e3f2f
zdu863 <zdu863@gmail.com>
2024-03-27 09:35:52 -0500

    changed unit tests

--- pyccl/baryons/baccoemu_baryons_dep.py
+++ pyccl/baryons/baccoemu_baryons_dep.py
@@ -11,6 +11,6 @@ class BaccoemuBaryons(BaryonsBaccoemu):
     name = 'BaccoemuBaryons'
     def __init__(self, *args, **kwargs):
         """This throws a deprecation warning on initialization."""
-        warn(f'{self.__class__.__name__} will be deprecated. Please use {BaryonsBaccoemu.__name__} instead.', DeprecationWarning, stacklevel=2)
+        warn(f'Class {self.__class__.__name__} will be deprecated. Please use {BaryonsBaccoemu.__name__} instead.', DeprecationWarning, stacklevel=2)
         super().__init__(*args, **kwargs)
     pass
