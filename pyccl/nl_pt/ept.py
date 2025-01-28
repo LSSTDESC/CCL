bebf0f7200a8d99667b621c143c978cb389508c6
Vincent Schacknies <vincent.schacknies@icloud.com>
2025-01-28 16:03:58 -0500

    change in get_pgi

--- pyccl/nl_pt/ept.py
+++ pyccl/nl_pt/ept.py
@@ -193,7 +193,8 @@ class EulerianPTCalculator(CCLAutoRepr):
         if( not hasattr(fpt, "IA_ta")):
             raise ValueError(f"Your FAST-PT version lacks a required attribute. You may have the wrong fast-pt install. Try running pip install fast-pt or conda install fast-pt, then try again")
         if (not hasattr(fpt, "IA_tij")):
-            raise ValueError(f"You are using an older version of FAST-PT, please run pip install fast-pt or conda install fast-pt, then try again")
+            pass
+            #raise ValueError(f"You are using an older version of FAST-PT, please run pip install fast-pt or conda install fast-pt, then try again")
         n_pad = int(self.fastpt_par['pad_factor'] * len(self.k_s))
         self.pt = fpt.FASTPT(self.k_s, to_do=to_do,
                              low_extrap=self.fastpt_par['low_extrap'],
@@ -455,7 +456,7 @@ class EulerianPTCalculator(CCLAutoRepr):
                             (self._g4*cd)[:,None] * (gb2dsij) +
                             (self._g4*c2)[:, None] * (gb2sij2) + 
                             (self._g4*ct)[:,None] * gb2tij) +
-               0.5*bs[:,None]*((self._g4*c1)[:,None]*Pd1s2 +
+               0.5*bs[:,None]*((self._g4*c1)[:,None]*Pd1s2+ #other term we tried: s2sij +
                             (self._g4*cd)[:,None] * (s2dsij) +
                             (self._g4*c2)[:,None] * (s2sij2) +
                             (self._g4*ct)[:, None] *s2tij) +
