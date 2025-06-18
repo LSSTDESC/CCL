4265074a581243e271db4cb70e2777a686bb4405
sambit-giri <sambit.giri@gmail.com>
2025-06-18 08:27:40 +0200

    init custom baryon suppression

---
+++ pyccl/baryons/baryons_custom.py
@@ -0,0 +1,89 @@
+__all__ = ("BaryonsCustom",)
+
+import numpy as np
+
+from .. import Pk2D
+from . import Baryons
+
+
+class BaryonsCustom(Baryons):
+    """The "BCM" model boost factor for baryons. BCM stands for the
+    "baryonic correction model" of `Schneider & Teyssier 2015
+    <https://arxiv.org/abs/1510.06034>`_. See the
+
+    The boost factor is applied multiplicatively so that
+    :math:`P_{\\rm bar.}(k, a) = P_{\\rm DMO}(k, a)\\, f_{\\rm BCM}(k, a)`.
+
+    Args:
+        boost_data (:obj:`array`): Array containing the boost factor data.
+        k_data (:obj:`array`): Wavenumber (in :math:`{\\rm Mpc}^{-1}`).
+        a_data (:obj:`array`): Scale factor.
+    """
+    name = 'BaryonsCustom'
+    __repr_attrs__ = __eq_attrs__ = ("boost_data", "k_data", "a_data")
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def boost_factor(self, cosmo, k, a):
+        """The BCM model boost factor for baryons.
+
+        Args:
+            cosmo (:class:`~pyccl.cosmology.Cosmology`): Cosmological parameters.
+            k (:obj:`float` or `array`): Wavenumber (in :math:`{\\rm Mpc}^{-1}`).
+            a (:obj:`float` or `array`): Scale factor.
+
+        Returns:
+            :obj:`float` or `array`: Correction factor to apply to \
+            the power spectrum.
+        """ # noqa
+        a_use, k_use = map(np.atleast_1d, [a, k])
+        a_use, k_use = a_use[:, None], k_use[None, :]
+
+        z = 1/a_use - 1
+        kh = k_use / cosmo['h']
+        b0 = 0.105*self.log10Mc - 1.27
+        bfunc = b0 / (1. + (z/2.3)**2.5)
+        kg = 0.7 * (1-bfunc)**4 * self.eta_b**(-1.6)
+        gf = bfunc / (1 + (kh/kg)**3) + 1. - bfunc
+        scomp = 1 + (kh / self.k_s)**2
+        fka = gf * scomp
+
+        if np.ndim(k) == 0:
+            fka = np.squeeze(fka, axis=-1)
+        if np.ndim(a) == 0:
+            fka = np.squeeze(fka, axis=0)
+        return fka
+
+    def update_parameters(self, log10Mc=None, eta_b=None, k_s=None):
+        """Update BCM parameters. All parameters set to ``None`` will
+        be left untouched.
+
+        Args:
+            log10Mc (:obj:`float`): logarithmic mass scale of hot
+                gas suppression.
+            eta_b (:obj:`float`): ratio of escape to ejection radii.
+            k_s (:obj:`float`): Characteristic scale (wavenumber) of
+                the stellar component.
+        """
+        if log10Mc is not None:
+            self.log10Mc = log10Mc
+        if eta_b is not None:
+            self.eta_b = eta_b
+        if k_s is not None:
+            self.k_s = k_s
+
+    def _include_baryonic_effects(self, cosmo, pk):
+        # Applies boost factor
+        a_arr, lk_arr, pk_arr = pk.get_spline_arrays()
+        k_arr = np.exp(lk_arr)
+        fka = self.boost_factor(cosmo, k_arr, a_arr)
+        pk_arr *= fka
+
+        if pk.psp.is_log:
+            np.log(pk_arr, out=pk_arr)  # in-place log
+
+        return Pk2D(a_arr=a_arr, lk_arr=lk_arr, pk_arr=pk_arr,
+                    is_logp=pk.psp.is_log,
+                    extrap_order_lok=pk.extrap_order_lok,
+                    extrap_order_hik=pk.extrap_order_hik)
