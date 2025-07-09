065e05c605d36487f5c2d9b229ecf54e33db643c
Keitaro Ishikawa <ishikawa@KeitaronoMacBook-Pro.local>
2025-07-09 13:35:57 +0100

    Fixed flake8 error etc.

--- pyccl/halos/emulators/darkemu1_pk.py
+++ pyccl/halos/emulators/darkemu1_pk.py
@@ -2,24 +2,26 @@ __all__ = ("darkemu_power_spectrum",)
 
 import warnings
 import numpy as np
-from scipy import integrate
+
 from scipy.interpolate import RectBivariateSpline as rbs
 from scipy.interpolate import InterpolatedUnivariateSpline as ius
 
-from ... import Pk2D
 from .. import Profile2pt
 
-from dark_emulator import darkemu
 from dark_emulator import model_hod
 hod = model_hod.darkemu_x_hod()
+
 
 def _compute_logdens(Mh, redshift):
     Mh = np.atleast_1d(Mh)
     logdens = np.log10([hod._convert_mass_to_dens(
-        Mh[i], redshift, integration=hod.config["hmf_int_algorithm"]) for i in range(len(Mh))])
+        Mh[i], redshift, integration=hod.config["hmf_int_algorithm"]) 
+        for i in range(len(Mh))])
     return logdens
 
-def _compute_p_hh(ks, Mh, redshift): # referred from dark_emulator_public/dark_emulator/model_hod/hod_interface.py
+
+# referred from dark_emulator_public/dark_emulator/model_hod/hod_interface.py
+def _compute_p_hh(ks, Mh, redshift):
     hod._compute_p_hh_spl(redshift)
 
     logdens_de = hod.g1.logdens_list
@@ -29,19 +31,20 @@ def _compute_p_hh(ks, Mh, redshift): # referred from dark_emulator_public/dark_e
         (len(logdens_de), len(Mh), len(ks)))
     for i in range(len(logdens_de)):
         p_hh_tmp[i] = rbs(-logdens_de, hod.fftlog_2h.k,
-                               hod.p_hh_base[i])(-logdens, ks)
+                          hod.p_hh_base[i])(-logdens, ks)
 
     p_hh = np.zeros((len(Mh), len(Mh), len(ks)))
     for i in range(len(Mh)):
         p_hh[:, i] = rbs(-logdens_de, hod.fftlog_2h.k,
-                              p_hh_tmp[:, i, :])(-logdens, ks)
+                         p_hh_tmp[:, i, :])(-logdens, ks)
     return p_hh.transpose(2, 0, 1)
 
 
-def _compute_p_hm(ks, Mh, redshift): # referred from dark_emulator_public/dark_emulator/model_hod/hod_interface.py
-    if hod.logdens_computed == False:
+# referred from dark_emulator_public/dark_emulator/model_hod/hod_interface.py
+def _compute_p_hm(ks, Mh, redshift):
+    if not hod.logdens_computed:
         hod._compute_logdens(redshift)
-    if hod.xi_hm_computed == False:
+    if not hod.xi_hm_computed:
         hod._compute_xi_hm(redshift)
 
     logdens = _compute_logdens(Mh, redshift)
@@ -63,7 +66,7 @@ def darkemu_power_spectrum(demu, cosmo, hmc, k, a, prof, *,
                            p_of_k_a=None,
                            get_1h=True, get_2h=True,
                            suppress_1h=None,
-                           extrap_pk=False): # KI add demu
+                           extrap_pk=False):
     """ Computes the halo model power spectrum for two
     quantities defined by their respective halo profiles.
     The halo model power spectrum for two profiles
@@ -109,10 +112,6 @@ def darkemu_power_spectrum(demu, cosmo, hmc, k, a, prof, *,
             defined as in `HMCODE-2020 <https://arxiv.org/abs/2009.01858>`_:
             :math:`1/[1+(k_*(a)/k)^4]`.
             If ``None`` the standard 1-halo term is returned with no damping.
-        extrap_pk (:obj:`bool`):
-            Whether to extrapolate ``p_of_k_a`` in case ``a`` is out of its
-            support. If ```False```, and the queried values are out of bounds,
-            an error is raised.
     
     Returns:
         (:obj:`float` or `array`): integral values evaluated at each
@@ -125,8 +124,9 @@ def darkemu_power_spectrum(demu, cosmo, hmc, k, a, prof, *,
     k_use = np.atleast_1d(k).astype(float)
 
     good_a = (1./a_use - 1. <= 1.48) & (1./a_use - 1. >= 0.)
-    if np.any(good_a == False):
-        warnings.warn('Dark Emulator I supports 0 <= z <= 1.48. The redshifts rather than 1.48 will be truncated.')
+    if np.any(good_a is False):
+        warnings.warn('Dark Emulator I supports 0 <= z <= 1.48. '
+                      'The redshifts rather than 1.48 will be truncated.')
     a_use = a_use[good_a]
 
     # KI add: cosmology set for the dark emulator
@@ -140,35 +140,32 @@ def darkemu_power_spectrum(demu, cosmo, hmc, k, a, prof, *,
                                  np.log(cosmo['A_s']*10**10),
                                  cosmo['n_s'],
                                  cosmo['w0']]))
-    
+
     if suppress_1h is not None:
         if not get_1h:
             raise ValueError("Can't suppress the 1-halo term "
                              "when get_1h is False.")
-    
+
     if prof2 is None:
         prof2 = prof
+        demu_phh_mass = demu.get_phh_mass
     if prof_2pt is None:
         prof_2pt = Profile2pt()
-    demu_phh_mass = demu.get_phh_mass
-    demu_phm_mass = demu.get_phm_mass
-    
-    # pk2d = cosmo.parse_pk(p_of_k_a)
-    extrap = cosmo if extrap_pk else None  # extrapolation rule for pk2d
-    
+        demu_phm_mass = demu.get_phm_mass
+
     na = len(a_use)
     nk = len(k_use)
     out = np.zeros([na, nk])
     M_array = hmc._mass.copy()
     good_m = (M_array <= 1E16 / h_) & (M_array >= 1E12 / h_)
     M_array = M_array[good_m]
-    if np.any(good_m == False):
-        warnings.warn('Dark Emulator I supports 1E12 <= Mh [Msun/h] <= 1E16. Others will be truncated.')
+    if np.any(good_m is False):
+        warnings.warn('Dark Emulator I supports 1E12 <= Mh [Msun/h] <= 1E16. '
+                      'Others will be truncated.')
     nM = len(M_array)
     print('nM', nM)
     lmass = np.log10(M_array)
     for ia, aa in enumerate(a_use):
-        
         # normalizations
         norm1 = prof.get_normalization(cosmo, aa, hmc=hmc)
         if prof2 == prof:
@@ -176,18 +173,18 @@ def darkemu_power_spectrum(demu, cosmo, hmc, k, a, prof, *,
         else:
             norm2 = prof2.get_normalization(cosmo, aa, hmc=hmc)
 
-        # call components: hmf, uk, 
-        # hmc._mf: dn/dlogM [Mpc^-3] (comoving) not dn/dM 
+        # call components: hmf, uk
+        # hmc._mf: dn/dlogM [Mpc^-3] (comoving) not dn/dM
         hmc._get_ingredients(cosmo, aa, get_bf=False)
         hmf = ius(hmc._lmass, hmc._mf)(lmass)
         u1k = prof.fourier(cosmo, k_use, M_array, aa).T
-        
+
         # calc pkhx
-        if prof2 == prof: # pkgg
+        if prof2 == prof:  # pkgg
             u2k = u1k
         # else:
         #     u2k = prof2.fourier(cosmo, k_use, M_array, aa).T
-        
+
             #########
             # pkhh
             # Dark Emulator accepts k [h/Mpc] and returns Pk [Mpc/h]^3 as units
@@ -195,45 +192,49 @@ def darkemu_power_spectrum(demu, cosmo, hmc, k, a, prof, *,
             for i in range(nM):
                 for j in range(i+1):
                     vals = demu_phh_mass(ks=k_use/h_,
-                                         M1=M_array[i]*h_,M2=M_array[j]*h_,
+                                         M1=M_array[i]*h_, M2=M_array[j]*h_,
                                          redshift=1./aa-1.) / h_**3.
-                    pkhh[:,i,j] = vals
-                    pkhh[:,j,i] = vals
-            # pkhh = _compute_p_hh(k_use, M_array, redshift=1./aa-1) # (Nk,NM,NM)
-            
+                    pkhh[:, i, j] = vals
+                    pkhh[:, j, i] = vals
+            # pkhh = _compute_p_hh(k_use, M_array, redshift=1./aa-1)
+            # (Nk,NM,NM)
+
             # integration
             pk_2h_M2_int = list()
             for i in range(nM):
                 pk_2h_M2_int.append(hmc._integrator(
-                    pkhh[:,i] * u1k * hmf[None,:], lmass))
-            pk_2h = hmc._integrator(np.array(pk_2h_M2_int).T * u2k * hmf[None,:], lmass)
+                    pkhh[:, i] * u1k * hmf[None, :], lmass))
+            pk_2h = hmc._integrator(np.array(pk_2h_M2_int).T * 
+                                    u2k * hmf[None, :], lmass)
             #########
 
         else:
             pkhm = np.zeros([nk, nM])
             for i in range(nM):
-                pkhm[:,i] = demu_phm_mass(ks=k_use/h_, M=M_array[i]*h_, redshift=1./aa-1.) / h_**3.
-            # pkhm = _compute_p_hm(k_use, M_array, redshift=1./aa-1) # (Nk,NM)
-            
-            pk_2h = hmc._integrator(pkhm * u1k * hmf[None,:], lmass)
-    
+                pkhm[:, i] = demu_phm_mass(ks=k_use/h_, M=M_array[i]*h_, 
+                                           redshift=1./aa-1.) / h_**3.
+            # pkhm = _compute_p_hm(k_use, M_array, redshift=1./aa-1)
+            # (Nk,NM)
+
+            pk_2h = hmc._integrator(pkhm * u1k * hmf[None, :], lmass)
+
         if get_1h and (prof2 == prof):
             pk_1h = hmc.I_0_2(cosmo, k_use, aa, prof,
                               prof2=prof2, prof_2pt=prof_2pt)  # 1h term
-    
+
             if suppress_1h is not None:
                 # large-scale damping of 1-halo term
                 ks = suppress_1h(aa)
                 pk_1h *= (k_use / ks)**4 / (1 + (k_use / ks)**4)
         else:
             pk_1h = 0
-    
+
         # smooth 1h/2h transition region
         if prof2 == prof:
             out[ia] = (pk_1h + pk_2h) / (norm1 * norm2)
         else:
             out[ia] = pk_2h / norm1
-    
+
     if np.ndim(a) == 0:
         out = np.squeeze(out, axis=0)
     if np.ndim(k) == 0:
