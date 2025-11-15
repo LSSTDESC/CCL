386c0218ec858d2b96809de31fbd3168d0236d23
niko.sarcevic <nikolina.sarcevic@gmail.com>
2025-11-15 14:42:45 +0000

    Fix FKEM imports and restore nonlimber_fkem as a proper package

--- pyccl/__init__.py
+++ pyccl/__init__.py
@@ -45,3 +45,7 @@ from . import halos
 from . import nl_pt
 
 from .cosmology import *
+
+from . import nonlimber_fkem as _nonlimber_fkem_pkg
+
+Cosmology.nonlimber_fkem = _nonlimber_fkem_pkg.core.nonlimber_fkem
