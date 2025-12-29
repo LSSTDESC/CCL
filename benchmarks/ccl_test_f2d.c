a135832b4a1b7986ca6a72077f751f7278adc8be
carlosggarcia <carlos.garcia-garcia@physics.ox.ac.uk>
2025-12-28 21:02:07 -0500

    added missing params to ccl_parameters_create

--- benchmarks/ccl_test_f2d.c
+++ benchmarks/ccl_test_f2d.c
@@ -287,10 +287,14 @@ CTEST2(f2d,pk) {
   ccl_configuration config = default_config;
   config.transfer_function_method = ccl_bbks;
   config.matter_power_spectrum_method = ccl_linear;
-  ccl_parameters params = ccl_parameters_create(data->Omega_c,data->Omega_b, 0.0, 3.046, &mnu,
-                                                0, -1.0, 0.0, data->h, data->A_s,data->n_s,
-                                                -1, -1, -1, 0., 0., 1.0, 1.0, 0.0,
-						-1, NULL, NULL, &status);
+  ccl_parameters params = ccl_parameters_create(data->Omega_c, data->Omega_b, 0.0,
+                                                3.046, &mnu, 0,
+                                                -1.0, 0.0, data->h, data->A_s, -1,
+                                                data->n_s, -1, 0.0, 0.0,
+                                                -1, -1, -1,
+                                                0.0, 0.0,
+                                                1.0, 1.0, 0.0,
+                                                -1, NULL, NULL, &status);
   params.T_CMB=2.7;
   params.Omega_k=0;
   params.Omega_g=0;
