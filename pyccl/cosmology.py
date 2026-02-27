bfa7b553095090e1ae2e3e1dd035d493c3e4bae5
Lhior <lmedinav@andrew.cmu.edu>
2026-02-27 13:24:19 -0500

    flake8-ed

--- pyccl/cosmology.py
+++ pyccl/cosmology.py
@@ -477,7 +477,8 @@ class Cosmology(CCLObject):
         # c1_mg = c2_mg = lambda_mg = 0.
         is_gr_case = (c1_mg == 1 and c2_mg == 1) or \
                      (c1_mg == 0 and c2_mg == 0 and lambda_mg == 0)
-        if not is_gr_case and self.transfer_function_type != 'boltzmann_isitgr':
+        if not is_gr_case and \
+                self.transfer_function_type != 'boltzmann_isitgr':
             raise ValueError(
                 "Your choice of c1_mg, c2_mg, and lambda_mg values is "
                 "inconsistent with your transfer function choice "
